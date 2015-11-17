3dadcce10119d355de4853b74e7e619fa0c72e5c
elena <litvin@nf.jinr.ru>
2015-11-17 16:16:43 +0400

    simplified version of BmnZdc added

---
+++ zdc/BmnZdcLinkDef.h
@@ -0,0 +1,13 @@
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class  BmnZdc+;
+#pragma link C++ class  BmnZdcPoint+;
+#pragma link C++ class  BmnZdcGeo+;
+#pragma link C++ class  BmnZdcGeoPar+;
+#pragma link C++ class  BmnZdcContFact+;
+#endif

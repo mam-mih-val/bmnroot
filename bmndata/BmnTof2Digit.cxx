5fcf6e33a103e29af1afa9bd22e2fceeae682e5a
Yuri Petukhov <Yuri.Petukhov@ihep.ru>
2016-01-04 19:29:07 +0300

    TOF2 analysis (DNL corrections, slewing etc.)

--- bmndata/BmnTof2Digit.cxx
+++ bmndata/BmnTof2Digit.cxx
@@ -4,18 +4,18 @@ BmnTof2Digit::BmnTof2Digit()
 {
    fPlane=-1;
    fStrip=-1;
-   fSide=-1;
    fAmplitude=-1;
    fTime=-1;
+   fDiff=-1;
 }
 
-BmnTof2Digit::BmnTof2Digit(Short_t plane, Short_t strip, Short_t side,Float_t t,Float_t a)
+BmnTof2Digit::BmnTof2Digit(Short_t plane, Short_t strip, Float_t t, Float_t a, Float_t d)
 {
    fPlane=plane;
    fStrip=strip;
-   fSide=side;
    fAmplitude=a;
    fTime=t;
+   fDiff=d;
 }
 
 BmnTof2Digit::~BmnTof2Digit()

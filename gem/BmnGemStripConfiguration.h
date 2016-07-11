05b3f963644706d7ceaf544541aafdaaade20237
dbaranov <dmitriybaranof@gmail.com>
2016-07-11 16:03:05 +0300

    GEM: ROOT-geometry of the GEM-configuration (summer run 2016)

--- gem/BmnGemStripConfiguration.h
+++ gem/BmnGemStripConfiguration.h
@@ -5,9 +5,10 @@ namespace BmnGemStripConfiguration {
     enum GEM_CONFIG {
         None = 0, // empty config
         Full, // 12 stations (geometry file: GEMS_v3.root)
-        First, // 6 stations (summer 2016) (geometry file: GEMS_1stConfig.root)
-        FirstShort, // 5 stations (summer 2016) - short version -(geometry file: GEMS_1stConfigShort.root)
-        Second // 7 stations (winter 2016) (geometry file: GEMS_2ndConfig.root)
+        First, // 6 stations (geometry file: GEMS_1stConfig.root)
+        FirstShort, // 5 stations - short version -(geometry file: GEMS_1stConfigShort.root)
+        Second, // 7 stations (geometry file: GEMS_2ndConfig.root)
+        RunSummer2016 // 7 stations (summer 2016) - (geometry file: GEMS_RunSummer2016.root)
     };
 }
 
@@ -41,6 +42,14 @@ namespace BmnGemStripPositions_SecondConfig {
     const Double_t XStationPositions[NStations] = {-2.2, +0.5, +0.7, +1.0, +1.4, +2.1, +2.9};
     const Double_t YStationPositions[NStations] = {+0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0};
     const Double_t ZStationPositions[NStations] = {30.0, 45.0, 60.0, 80.0, 100.0, 130.0, 160.0};
+}
+
+namespace BmnGemStripPositions_RunSummer2016 {
+    //BM@N coordinate system is used
+    const Int_t NStations = 7;
+    const Double_t XStationPositions[NStations] = {+0.0, +1.5, -1.5, +1.5, -1.5, +1.5, +0.0};
+    const Double_t YStationPositions[NStations] = {+0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0};
+    const Double_t ZStationPositions[NStations] = {5.0+0.1, 41.8+0.7, 62.8+0.7, 83.8+0.7, 104.8+0.7, 125.8+0.7, 146.8+1.5}; //All z-positions are start pos. of the sensitive vol.
 }
 
 #endif

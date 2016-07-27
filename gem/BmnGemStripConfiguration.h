0f11c97f4de7ce8745585884487ec2b88b34b9d6
dbaranov <dmitriybaranof@gmail.com>
2016-07-27 09:08:51 +0300

    GEM: corrected x-shifts for each GEM station (geometry+configuration)

--- gem/BmnGemStripConfiguration.h
+++ gem/BmnGemStripConfiguration.h
@@ -47,7 +47,7 @@ namespace BmnGemStripPositions_SecondConfig {
 namespace BmnGemStripPositions_RunSummer2016 {
     //BM@N coordinate system is used
     const Int_t NStations = 7;
-    const Double_t XStationPositions[NStations] = {+0.0, +1.5, -1.5, +1.5, -1.5, +1.5, +0.0};
+    const Double_t XStationPositions[NStations] = {+0.0, -1.5, +1.5, -1.5, +1.5, -1.5, +0.0};
     const Double_t YStationPositions[NStations] = {+0.0, +0.0, +0.0, +0.0, +0.0, +0.0, +0.0};
     const Double_t ZStationPositions[NStations] = {5.0+0.1, 41.8+0.7, 62.8+0.7, 83.8+0.7, 104.8+0.7, 125.8+0.7, 146.8+1.5}; //All z-positions are start pos. of the sensitive vol.
 }

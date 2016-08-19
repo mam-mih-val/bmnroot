804ac225c5e0f38a06a6182bfa3b574ba7ae5947
Pavel Batyuk <pavel.batyuk@jinr.ru>
2016-08-19 12:24:50 +0300

    BM@N alignment package, development

--- macro/alignment/test_alignment.C
+++ macro/alignment/test_alignment.C
@@ -2,16 +2,18 @@ void test_alignment(TString fileNumber = "All") {
  gROOT->LoadMacro("$VMCWORKDIR/macro/run/bmnloadlibs.C");
  bmnloadlibs(); // load BmnRoot libraries
 
- BmnGemAlignment* gemAlign = new BmnGemAlignment(TString("bmn_run00" + fileNumber + "_digi.root").Data(), TString("reco_" + fileNumber + ".root").Data());
- gemAlign->SetDebugInfo(kTRUE);
- gemAlign->SetNofEvents(30000);
+ Bool_t useMilleOnly = false;
+ BmnGemAlignment* gemAlign = new BmnGemAlignment(TString("/nfs/digits_run4/bmn_run00" + fileNumber + "_digi.root").Data(), TString("reco_" + fileNumber + ".root").Data(), useMilleOnly);
+  // gemAlign->SetDebugInfo(kTRUE);
+ gemAlign->SetNofEvents(10000);
  gemAlign->SetMaxNofHitsPerEvent(30);
- gemAlign->SetSignalToNoise(1000., 2., 1000., 2., 1000., 2., 2.); // 1000 is an artificial threshold not to use a station
+ gemAlign->SetSignalToNoise(1000., -2., -3., -2., -3., -2., 1000.); // 1000 is an artificial threshold not to use a station
  gemAlign->SetChi2Max(0.1);
  gemAlign->SetThreshold(0.);
- gemAlign->SetMinHitsAccepted(3);
- gemAlign->SetTxMinMax(-0.05, 0.05);
- gemAlign->SetTyMinMax(-0.05, 0.05);
+ gemAlign->SetMinHitsAccepted(4);
+ gemAlign->SetMaxHitsAccepted(6);
+ // gemAlign->SetTxMinMax(-0.05, 0.05);
+ // gemAlign->SetTyMinMax(-0.05, 0.05);
  gemAlign->SetAlignmentDim("xy");
  gemAlign->SetSteerFile("steer.txt");
  gemAlign->PrepareData();

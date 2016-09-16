8195822223b0a8a0c54fddbc58dbfe7c4f3bef32
Lobastov S.P <lsp@lnr.jinr.ru>
2016-09-16 09:59:11 +0300

    new tof1 layout

--- macro/run/geometry.C
+++ macro/run/geometry.C
@@ -42,7 +42,7 @@ geometry(FairRunSim *fRun) {
 
     FairDetector* tof1 = new BmnTOF1("TOF1", kTRUE);
     //  tof1->SetGeometryFileName("tof1_W400_v1.geo");
-    tof1->SetGeometryFileName("TOFB1_v2.root");
+    tof1->SetGeometryFileName("TOFB1_v3.root");
     fRun->AddModule(tof1);
 
     FairDetector* dch1 = new BmnDch1("DCH1", kTRUE);

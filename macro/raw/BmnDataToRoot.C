3bd672afa5be63dde1688d0a8b7846a96417fdf4
Sergei Merts <>
2016-09-27 14:11:40 +0300

    Small corrections

--- macro/raw/BmnDataToRoot.C
+++ macro/raw/BmnDataToRoot.C
@@ -1,15 +1,14 @@
 
-void BmnDataToRoot(char *file, Long_t nEvents = 0) {
+void BmnDataToRoot(TString file, Long_t nEvents = 0) {
   gROOT->LoadMacro("$VMCWORKDIR/macro/run/bmnloadlibs.C");
   bmnloadlibs(); // load BmnRoot libraries
 
   //put here path to raw data
   TString pathToRun4 = "/home/merz/BMN/dataRun4/";
   
-  BmnRawDataDecoder* decoder = new BmnRawDataDecoder(pathToRun4 + TString(file), nEvents);
+  BmnRawDataDecoder* decoder = new BmnRawDataDecoder(pathToRun4 + file, nEvents);
   decoder->SetPedestalRun(kFALSE);
   decoder->SetTrigMapping("Trig_map_Run4.txt");
-  decoder->SetTof400Mapping("");
   decoder->SetTof700Mapping("");
   decoder->ConvertRawToRoot();  // Convert raw data in .data format into adc-,tdc-, ..., sync-digits in .root format
   decoder->DecodeDataToDigi();  // Decode data into detector-digits using current mappings.

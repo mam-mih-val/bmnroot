f16a85974ba622a2f7873feba95263832dfc8a0a
Pavel Batyuk <pavel.batyuk@jinr.ru>
2016-12-12 16:40:10 +0300

    Code modifications related to the Millepede usage (some extended options are introduced to the package). Input data are taken from the "official" tracking procedure.

--- macro/alignment/gemAlignment.C
+++ macro/alignment/gemAlignment.C
@@ -1,10 +1,7 @@
 #include <TString.h>
 #include <TStopwatch.h>
 
-//void gemAlignment(Int_t fileNumber = 65, Int_t nEvents = 10000) {
-void gemAlignment(Int_t fileNumber = 65, Int_t nEvents = 10000, TString addInfo = "") {
-    TString type = (fileNumber < 66) ? "beam" : "target";
-
+void gemAlignment(UInt_t nEvents = 1e6, TString recoFileName = "bmndst_noCorr_Stat_12345.root") {
     gROOT->LoadMacro("$VMCWORKDIR/macro/run/bmnloadlibs.C");
     bmnloadlibs(); // load BmnRoot libraries
 
@@ -14,25 +11,34 @@ void gemAlignment(Int_t fileNumber = 65, Int_t nEvents = 10000, TString addInfo 
 
     FairRunAna *fRun = new FairRunAna();
     FairSource* fFileSource;
-    TString num = "";
-    // TString addInfo = "";
-    num += fileNumber;
-  //fFileSource = new BmnFileSource(TString("reco_" + num + ".root").Data());
-    fFileSource = new BmnFileSource(TString("bmn_run00"+num+addInfo+"_reco.root").Data());
+    fFileSource = new BmnFileSource(recoFileName.Data());
     fRun->SetSource(fFileSource);
     fRun->SetOutputFile("align.root");
 
     BmnGemAlignment* gemAlign = new BmnGemAlignment();
-    /****DEFINE RECO CONFIG*****/
-    TString fixedStats[7] = {"", "fixed", "fixed", "", "fixed", "fixed", ""}; // St. 6 consists of two separate modules that could be either fixed or not simultaneously
+    TString type = "beam";
+
+    TString fixedStats[7] = {"fixed", "", "", "", "", "", "fixed"}; // St. 6 consists of two separate modules that could be either fixed or not simultaneously
     gemAlign->SetStatNumFixed(fixedStats);
     gemAlign->SetRunType(type);
     gemAlign->SetPreSigma(0.01); // Default value is 1
     gemAlign->SetAccuracy(1e-2); // Default value is 1e-3
-    gemAlign->SetNumIterations(10);
-    // gemAlign->SetDebugInfo(kTRUE);
-    // gemAlign->SetAlignmentTypeByHands("xy"); // Can be either xy or xyz
+//     gemAlign->SetUseRealHitErrors(kFALSE); // Default value is true
+    gemAlign->SetUseRegularization(kTRUE); // Default value is false
+    gemAlign->SetHugecut(20.); // Default value is 50
+    gemAlign->SetChisqcut(5., 1.); //Default value is (0., 0.)
+    gemAlign->SetEntriesPerParam(300); //Defaul value is 10
+    gemAlign->SetOutlierdownweighting(4); // Default value is 0
+    gemAlign->SetDwfractioncut(0.3); // Default value is 0, should be less than 0.5 
+    // gemAlign->SetDebugInfo(kTRUE);      // Default value is false
+    // gemAlign->SetAlignmentTypeByHands("xy");
 
+    // Restrictions on track params.
+    // gemAlign->SetMinHitsAccepted(3); // Default value is 3
+   // gemAlign->SetChi2MaxPerNDF(2.); // Cut on chi2/ndf for found tracks, default value is not limited
+    gemAlign->SetTxMinMax(-0.005, 0.005);
+    gemAlign->SetTyMinMax(-0.005, 0.005);
+ 
     /********************************/
 
     fRun->AddTask(gemAlign);

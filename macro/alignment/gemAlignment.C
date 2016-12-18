65e0b23eccdcb41312391fa90cc612cebea0de9e
Pavel Batyuk <pavel.batyuk@jinr.ru>
2016-12-18 19:05:21 +0300

    A possibility to choose a single track with the minimum chi2-value added. It seems to be reasonable in case of d(TARGET)-interactions.

--- macro/alignment/gemAlignment.C
+++ macro/alignment/gemAlignment.C
@@ -1,8 +1,7 @@
 #include <TString.h>
 #include <TStopwatch.h>
 
-void gemAlignment_new(UInt_t nEvents = 1e6, TString recoFileName = "bmn_run05_Glob_0448_bmndst.root", TString addInfo = "")
-{
+void gemAlignment(UInt_t nEvents = 1e6, TString recoFileName = "bmndst_573_all.root", TString addInfo = "") {
     gROOT->LoadMacro("$VMCWORKDIR/macro/run/bmnloadlibs.C");
     bmnloadlibs(); // load BmnRoot libraries
     // -----   Timer   --------------------------------------------------------
@@ -17,11 +16,10 @@ void gemAlignment_new(UInt_t nEvents = 1e6, TString recoFileName = "bmn_run05_Gl
     TRegexp re("bmndst");
     if (addInfo == "") {
         outputFile(re) = "align"; // replace bmndst with align
+    } else {
+        outputFile(re) = addInfo + "_align";
     }
-    else {
-        outputFile(re) = addInfo+"_align";
-    }
-  //fRun->SetOutputFile("align.root");
+    //fRun->SetOutputFile("align.root");
     fRun->SetOutputFile(outputFile);
     TString resultName = outputFile;
     re = "_align.root";
@@ -29,27 +27,28 @@ void gemAlignment_new(UInt_t nEvents = 1e6, TString recoFileName = "bmn_run05_Gl
     BmnGemAlignment* gemAlign = new BmnGemAlignment();
     gemAlign->SetResultName(resultName);
     gemAlign->SetGeometry(BmnGemStripConfiguration::RunWinter2016);
-    TString type = "beam";
+    TString type = "target";
     TString fixedStats[7] = {"fixed", "", "", "", "", "", ""}; // St. 4, 5 and 6 consist of two separate modules that could be either fixed or not simultaneously
     gemAlign->SetStatNumFixed(fixedStats);
     gemAlign->SetRunType(type);
-    gemAlign->SetPreSigma(0.001);                  // Default value is 1
-    gemAlign->SetAccuracy(1e-3);                   // Default value is 1e-3
-    gemAlign->SetUseRealHitErrors(kTRUE);          // Default value is false
-    gemAlign->SetUseRegularization(kTRUE);         // Default value is false
-    gemAlign->SetHugecut(20.);                     // Default value is 50
-  //gemAlign->SetChisqcut(5., 2.);                 // Default value is (0., 0.)
-  //gemAlign->SetEntriesPerParam(3);               // Default value is 10
-    gemAlign->SetOutlierdownweighting(4);          // Default value is 0
-    gemAlign->SetDwfractioncut(0.3);               // Default value is 0, should be less than 0.5
+    gemAlign->SetPreSigma(0.001); // Default value is 1
+    gemAlign->SetAccuracy(1e-3); // Default value is 1e-3
+    gemAlign->SetUseRealHitErrors(kTRUE); // Default value is false
+    // gemAlign->SetUseTrackWithMinChi2(kTRUE);       // Default value is false
+    gemAlign->SetUseRegularization(kTRUE); // Default value is false
+    gemAlign->SetHugecut(20.); // Default value is 50
+    // gemAlign->SetChisqcut(5., 2.); // Default value is (0., 0.)
+    gemAlign->SetEntriesPerParam(100); // Default value is 10
+    gemAlign->SetOutlierdownweighting(4); // Default value is 0
+    gemAlign->SetDwfractioncut(0.5); // Default value is 0, should be less than 0.5
     gemAlign->SetFixDetector(kTRUE, kTRUE, kTRUE); // Default values are false(X), false(Y) and false(Z)
-  //gemAlign->SetDebugInfo(kTRUE);                 // Default value is false
-  //gemAlign->SetAlignmentTypeByHands("xyz");
-  //Restrictions on track params.
-  //gemAlign->SetMinHitsAccepted(3);               // Default value is 3
-  //gemAlign->SetChi2MaxPerNDF(2.);                // Cut on chi2/ndf for found tracks, default value is not limited
-  //gemAlign->SetTxMinMax(-0.005, 0.005);
-  //gemAlign->SetTyMinMax(-0.005, 0.005);
+    //gemAlign->SetDebugInfo(kTRUE);                 // Default value is false
+    //gemAlign->SetAlignmentTypeByHands("xyz");
+    //Restrictions on track params.
+    //gemAlign->SetMinHitsAccepted(3);               // Default value is 3
+    //gemAlign->SetChi2MaxPerNDF(2.);                // Cut on chi2/ndf for found tracks, default value is not limited
+    //gemAlign->SetTxMinMax(-0.005, 0.005);
+    //gemAlign->SetTyMinMax(-0.005, 0.005);
     /********************************/
     fRun->AddTask(gemAlign);
     fRun->Init();

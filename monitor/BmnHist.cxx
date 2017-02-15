3fa858d8582d4ed4caff81a51f0dcf6533fbefca
Ilnur Gabdrakhmanov <ilnur@jinr.ru>
2017-02-15 16:07:25 +0300

    forgotten new class added

---
+++ monitor/BmnHist.cxx
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   BmnHist.cxx
+ * Author: ilnur
+ * 
+ * Created on February 2, 2017, 2:10 PM
+ */
+
+#include "BmnHist.h"
+
+BmnHist::BmnHist() {
+    refFile = NULL;
+    frecoTree = NULL;
+}
+
+
+BmnHist::~BmnHist() {
+}
+
+ClassImp(BmnHist);

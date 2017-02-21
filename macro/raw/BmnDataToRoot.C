c8a46eb16463c87c3cfd6545e2feb8bad9f092cc
Yuri Petukhov <Yuri.Petukhov@ihep.ru>
2017-02-21 15:01:04 +0300

    Add ECAL stuff to script

--- macro/raw/BmnDataToRoot.C
+++ macro/raw/BmnDataToRoot.C
@@ -3,7 +3,7 @@
 //nEvents: if 0 then decode all events
 //doConvert: convert RAW --> ROOT before decoding or use file converted before
 
-void BmnDataToRoot(TString file, Long_t nEvents = 0, Bool_t doConvert = kTRUE)
+void BmnDataToRoot(TString file = "mpd_run_Glob_814.data", Long_t nEvents = 0, Bool_t doConvert = kFALSE)
 {
     gROOT->LoadMacro("$VMCWORKDIR/macro/run/bmnloadlibs.C");
     bmnloadlibs(); // load BmnRoot libraries
@@ -16,6 +16,8 @@ void BmnDataToRoot(TString file, Long_t nEvents = 0, Bool_t doConvert = kTRUE)
     decoder->SetTof700Mapping("TOF700_map_period_5.txt");
     decoder->SetZDCMapping("ZDC_map_period_5.txt");
     decoder->SetZDCCalibration("zdc_muon_calibration.txt");
+    decoder->SetECALMapping("ECAL_map_period_5.txt");
+    decoder->SetECALCalibration("");
     decoder->SetMwpcMapping("MWPC_mapping_period_5.txt");
     if (doConvert) decoder->ConvertRawToRoot(); // Convert raw data in .data format into adc-,tdc-, ..., sync-digits in .root format
     decoder->DecodeDataToDigi(); // Decode data into detector-digits using current mappings.

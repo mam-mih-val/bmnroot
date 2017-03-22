5e67354363382bb1aad7246a54b61610a56718ae
soul <soul@se51-99.jinr.ru>
2017-03-22 12:15:40 +0300

    change field_voltage position in DB for comfort; new sql function to clean unused geometry

--- uni_db/scheme/uni_db.sql
+++ uni_db/scheme/uni_db.sql
@@ -628,36 +628,6 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-
-CREATE OR REPLACE FUNCTION clean_large_object() RETURNS integer AS $$
-DECLARE
-  geo run_geometry%rowtype;
-  par detector_parameter%rowtype;
-  objID integer;
-BEGIN
-  EXECUTE 'CREATE TABLE lo_current_list (lo_id int)';
-  FOR geo IN SELECT * FROM run_geometry LOOP
-    objID = geo.root_geometry;
-    insert into lo_current_list(lo_id) values (objID);
-  END LOOP;
-  FOR par IN SELECT * FROM detector_parameter LOOP
-    objID = par.parameter_value;
-    insert into lo_current_list(lo_id) values (objID);
-  END LOOP;
-  
-  FOR objID IN SELECT distinct loid FROM pg_largeobject LOOP
-    IF NOT EXISTS (SELECT 1 FROM lo_current_list WHERE lo_id = objID) THEN
-      RAISE INFO 'Unused: %', objID;
-      PERFORM lo_unlink(objID);
-    END IF;
-  END LOOP;
-
-  EXECUTE 'drop table lo_current_list'; 
-  RETURN 0;
-END;
-$$ LANGUAGE plpgsql;
---select "clean_large_object"();
-
 -- drop function lo_size(int)
 CREATE OR REPLACE FUNCTION lo_size(bytea) RETURNS integer
 AS $$
@@ -700,6 +670,51 @@ begin
   return sz;
 end;
 $$ LANGUAGE 'plpgsql';
+
+CREATE OR REPLACE FUNCTION clean_large_object() RETURNS integer AS $$
+DECLARE
+  geo run_geometry%rowtype;
+  par detector_parameter%rowtype;
+  objID integer;
+BEGIN
+  EXECUTE 'CREATE TABLE lo_current_list (lo_id int)';
+  FOR geo IN SELECT * FROM run_geometry LOOP
+    objID = geo.root_geometry;
+    insert into lo_current_list(lo_id) values (objID);
+  END LOOP;
+  FOR par IN SELECT * FROM detector_parameter LOOP
+    objID = par.parameter_value;
+    insert into lo_current_list(lo_id) values (objID);
+  END LOOP;
+  
+  FOR objID IN SELECT distinct loid FROM pg_largeobject LOOP
+    IF NOT EXISTS (SELECT 1 FROM lo_current_list WHERE lo_id = objID) THEN
+      RAISE INFO 'Unused: %', objID;
+      PERFORM lo_unlink(objID);
+    END IF;
+  END LOOP;
+
+  EXECUTE 'drop table lo_current_list'; 
+  RETURN 0;
+END;
+$$ LANGUAGE plpgsql;
+--select "clean_large_object"();
+
+CREATE OR REPLACE FUNCTION clean_unused_geometry() RETURNS integer AS $$
+DECLARE
+  geometryID integer;
+BEGIN  
+  FOR geometryID IN SELECT geometry_id FROM run_geometry LOOP
+    IF NOT EXISTS (SELECT 1 FROM run_ WHERE geometry_id = geometryID) THEN
+      RAISE INFO 'Unused geometry: %', geometryID;
+      delete from run_geometry where geometry_id = geometryID;
+    END IF;
+  END LOOP;
+
+  RETURN 0;
+END;
+$$ LANGUAGE plpgsql;
+--select "clean_unused_geometry"();
 
 
 -- DEFAULT INSERTION

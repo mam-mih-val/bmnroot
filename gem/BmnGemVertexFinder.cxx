5ddc83b899e11d0326cb454646dc71e03170a12d
Sergei Merts <>
2017-06-29 12:21:29 +0300

    Vertex finder update

--- gem/BmnGemVertexFinder.cxx
+++ gem/BmnGemVertexFinder.cxx
@@ -1,6 +1,9 @@
 
 #include "BmnGemVertexFinder.h"
 #include "BmnMath.h"
+#include "TGraph.h"
+#include "TCanvas.h"
+#include "TFitResult.h"
 #include "vector"
 
 static Float_t workTime = 0.0;
@@ -60,148 +63,6 @@ void BmnGemVertexFinder::Exec(Option_t* opt) {
     else
         new((*fVertexArray)[fVertexArray->GetEntriesFast()]) CbmVertex("vertex", "vertex", -1000.0, -1000.0, -1000.0, 0.0, 0, fNTracks, TMatrixFSym(3));
 
-    //    if (fNTracks > 0 && fNTracks < 2) { //one-track events
-    //        //Just propagation track on Z = 0.0
-    //        const Float_t fZ = -21.7; //fixed target position (NOT GOOD!)
-    //        Float_t fX, fY;
-    //        BmnGemTrack* track = (BmnGemTrack*) fGemTracksArray->At(0);
-    //        if (track->GetFlag() != kBMNBAD) {
-    //            FairTrackParam par0 = *(track->GetParamFirst());
-    //            if (!fIsField) {
-    //                BmnGemStripHit* hit = (BmnGemStripHit*) fGemHitsArray->At(track->GetHitIndex(0));
-    //                fX = hit->GetX() - par0.GetTx() * hit->GetZ();
-    //                fY = hit->GetY() - par0.GetTy() * hit->GetZ();
-    //            } else {
-    //                fKalman = new BmnKalmanFilter_tmp();
-    //                vector<Double_t>* F = new vector<Double_t> (25, 0.);
-    //                if (F != NULL) {
-    //                    F->assign(25, 0.);
-    //                    (*F)[0] = 1.;
-    //                    (*F)[6] = 1.;
-    //                    (*F)[12] = 1.;
-    //                    (*F)[18] = 1.;
-    //                    (*F)[24] = 1.;
-    //                }
-    //                fKalman->TGeoTrackPropagate(&par0, fZ, pdg, F, 0, "field");
-    //                fX = par0.GetX();
-    //                fY = par0.GetY();
-    //
-    //                delete F;
-    //                delete fKalman;
-    //            }
-    //            new((*fVertexArray)[fVertexArray->GetEntriesFast()]) CbmVertex("vertex", "vertex", fX, fY, fZ, 0.0, 0, 1, TMatrixFSym(3));
-    //        }
-    //    } else if (fNTracks >= 2) {
-    //        const Float_t z0 = -50.0;
-    //        const Float_t z1 = 50.0;
-    //
-    //        Float_t l1(0.0), l2(0.0), l3(0.0), l4(0.0);
-    //        Float_t t1(0.0), t2(0.0), t3(0.0), t4(0.0);
-    //        Float_t k1(0.0), k2(0.0), k3(0.0), k4(0.0);
-    //
-    //        for (Int_t iTr = 0; iTr < fNTracks; ++iTr) {
-    //            BmnGemTrack* track = (BmnGemTrack*) fGemTracksArray->At(iTr);
-    //            if (track->GetFlag() == kBMNBAD) continue;
-    //            FairTrackParam par0 = *(track->GetParamFirst());
-    //            Float_t Xf = par0.GetX();
-    //            Float_t Yf = par0.GetY();
-    //            Float_t Zf = par0.GetZ();
-    //            Float_t Tx = par0.GetTx();
-    //            Float_t Ty = par0.GetTy();
-    //
-    //            Float_t x0, y0;
-    //            Float_t x1, y1;
-    //
-    //            if (!fIsField) {
-    //                x0 = Tx * (z0 - Zf) + Xf;
-    //                y0 = Ty * (z0 - Zf) + Yf;
-    //                x1 = Tx * (z1 - Zf) + Xf;
-    //                y1 = Ty * (z1 - Zf) + Yf;
-    //            } else {
-    //                FairTrackParam par1 = *(track->GetParamFirst());
-    //                fKalman = new BmnKalmanFilter_tmp();
-    //                vector<Double_t>* F = new vector<Double_t> (25, 0.);
-    //                if (F != NULL) {
-    //                    F->assign(25, 0.);
-    //                    (*F)[0] = 1.;
-    //                    (*F)[6] = 1.;
-    //                    (*F)[12] = 1.;
-    //                    (*F)[18] = 1.;
-    //                    (*F)[24] = 1.;
-    //                }
-    //
-    //                fKalman->TGeoTrackPropagate(&par0, z0, pdg, F, 0, "field");
-    //                fKalman->TGeoTrackPropagate(&par1, z1, pdg, F, 0, "field");
-    //
-    //                x0 = par0.GetX();
-    //                y0 = par0.GetY();
-    //                x1 = par1.GetX();
-    //                y1 = par1.GetY();
-    //
-    //                delete F;
-    //                delete fKalman;
-    //            }
-    //
-    //            if (x1 == x0) continue;
-    //
-    //            Float_t a1 = (y1 - y0) / (x1 - x0);
-    //            Float_t a2 = (x1 - x0) / (z1 - z0);
-    //            Float_t a3 = (y1 - y0) / (z1 - z0);
-    //
-    //            Float_t b1 = y0 - a1 * x0;
-    //            Float_t b2 = x0 - a2 * z0;
-    //            Float_t b3 = y0 - a3 * z0;
-    //
-    //            Float_t A1 = 1 / (1 + a1 * a1);
-    //            Float_t A2 = 1 / (1 + a2 * a2);
-    //            Float_t A3 = 1 / (1 + a3 * a3);
-    //
-    //            l1 += (a1 * a1 * A1 + A2);
-    //            l2 += (-a1 * A1);
-    //            l3 += (-a2 * A2);
-    //            l4 += (a1 * b1 * A1 - b2 * A2);
-    //
-    //            t1 += (-a1 * A1);
-    //            t2 += (A1 + A3);
-    //            t3 += (-a3 * A3);
-    //            t4 += (-b1 * A1 - b3 * A3);
-    //
-    //            k1 += (-a2 * A2);
-    //            k2 += (-a3 * A3);
-    //            k3 += (a2 * a2 * A2 + a3 * a3 * A3);
-    //            k4 += (a2 * b2 * A2 + a3 * b3 * A3);
-    //        }
-    //
-    //        vz = (l2 * (t4 * k1 - k4 * t1) + l1 * (t2 * k4 - k2 * t4) + l4 * (t1 * k2 - k1 * t2)) / (l2 * (t3 * k1 - k3 * t1) + l1 * (t2 * k3 - k2 * t3) + l3 * (t1 * k2 - k1 * t2));
-    //        vy = (t4 * l1 - t1 * l4 - vz * (l1 * t3 - t1 * l3)) / (l1 * t2 - t1 * l2);
-    //        vx = (l4 - vz * l3 - vy * l2) / l1;
-    //
-    //        //        for (Int_t iTr = 0; iTr < nTracks; ++iTr) {
-    //        //            BmnGemTrack* track = (BmnGemTrack*) fGemTracksArray->At(iTr);
-    //        //            if (Abs(track->GetChi2()) > 50) continue;
-    //        //            FairTrackParam par0 = *(track->GetParamFirst());
-    //        //            fKalman = new BmnKalmanFilter_tmp();
-    //        //            vector<Double_t>* F = new vector<Double_t> (25, 0.);
-    //        //            if (F != NULL) {
-    //        //                F->assign(25, 0.);
-    //        //                (*F)[0] = 1.;
-    //        //                (*F)[6] = 1.;
-    //        //                (*F)[12] = 1.;
-    //        //                (*F)[18] = 1.;
-    //        //                (*F)[24] = 1.;
-    //        //            }
-    //        //
-    //        //            TString type = (fIsField) ? "field" : "line";
-    //        //            fKalman->TGeoTrackPropagate(&par0, vz, pdg, F, 0, type);
-    //        //            Float_t xi = par0.GetX();
-    //        //            Float_t yi = par0.GetY();
-    //        //            track->SetB(Sqrt((yi - vy) * (yi - vy) + (xi - vx) * (xi - vx)));
-    //        //            delete F;
-    //        //            delete fKalman;
-    //        //        }
-    //
-    //        new((*fVertexArray)[fVertexArray->GetEntriesFast()]) CbmVertex("vertex", "vertex", vx, vy, vz, 0.0, 0, fNTracks, TMatrixFSym(3));
-    //    }
     if (fVerbose) cout << "\n======================== Vertex finder exec finished ======================" << endl;
 
     clock_t tFinish = clock();
@@ -212,13 +73,21 @@ Float_t BmnGemVertexFinder::FindVZByVirtualPlanes() {
 
     fKalman = new BmnKalmanFilter_tmp();
 
-    vector<TVector3> Plane0Hits;
-    vector<TVector3> Plane1Hits;
-    vector<TVector3> Plane2Hits;
+    const Int_t nPlanes = 200;
+    const Float_t zMax = -10.0;
+    const Float_t zMin = -30.0;
+    const Float_t zStep = (zMax - zMin) / nPlanes;
 
-    Float_t z0 = 0.0;//fDetector->GetGemStation(0)->GetZPosition();
-    Float_t z1 = -25.0;
-    Float_t z2 = -50.0;//-z0;
+    vector<TVector3> PlaneHits[nPlanes];
+    Float_t zPlane[nPlanes];
+    Float_t dist[nPlanes];
+
+    for (Int_t iPlane = 0; iPlane < nPlanes; ++iPlane) {
+        zPlane[iPlane] = zMax - iPlane * zStep;
+        dist[iPlane] = 0.0;
+    }
+
+    TString propagationType = (fIsField) ? "field" : "line";
 
     for (Int_t iTr = 0; iTr < fNTracks; ++iTr) {
         BmnGemTrack* track = (BmnGemTrack*) fGemTracksArray->At(iTr);
@@ -234,42 +103,43 @@ Float_t BmnGemVertexFinder::FindVZByVirtualPlanes() {
             (*F)[18] = 1.;
             (*F)[24] = 1.;
         }
-
-        TString propagationType = (fIsField) ? "field" : "line";
-        fKalman->TGeoTrackPropagate(&par0, z0, 2212, F, NULL, propagationType);
-        TVector3 v0 = TVector3(par0.GetX(), par0.GetY(), par0.GetZ());
-        Plane0Hits.push_back(v0);
-
-        fKalman->TGeoTrackPropagate(&par0, z1, 2212, F, NULL, propagationType);
-        TVector3 v1 = TVector3(par0.GetX(), par0.GetY(), par0.GetZ());
-        Plane1Hits.push_back(v1);
-
-        fKalman->TGeoTrackPropagate(&par0, z2, 2212, F, NULL, propagationType);
-        TVector3 v2 = TVector3(par0.GetX(), par0.GetY(), par0.GetZ());
-        Plane2Hits.push_back(v2);
+        for (Int_t iPlane = 0; iPlane < nPlanes; ++iPlane) {
+            fKalman->TGeoTrackPropagate(&par0, zPlane[iPlane], 2212, F, NULL, propagationType);
+            PlaneHits[iPlane].push_back(TVector3(par0.GetX(), par0.GetY(), par0.GetZ()));
+        }
     }
 
-    Float_t d0 = 0.0;
-    Float_t d1 = 0.0;
-    Float_t d2 = 0.0;
-    Int_t nPairs = 0;
-
-    for (Int_t i = 0; i < fNTracks; ++i)
-        for (Int_t j = i + 1; j < fNTracks; ++j) {
-            d0 += Sqrt((Plane0Hits[i].X() - Plane0Hits[j].X()) * (Plane0Hits[i].X() - Plane0Hits[j].X()) + (Plane0Hits[i].Y() - Plane0Hits[j].Y()) * (Plane0Hits[i].Y() - Plane0Hits[j].Y()));
-            d1 += Sqrt((Plane1Hits[i].X() - Plane1Hits[j].X()) * (Plane1Hits[i].X() - Plane1Hits[j].X()) + (Plane1Hits[i].Y() - Plane1Hits[j].Y()) * (Plane1Hits[i].Y() - Plane1Hits[j].Y()));
-            d2 += Sqrt((Plane2Hits[i].X() - Plane2Hits[j].X()) * (Plane2Hits[i].X() - Plane2Hits[j].X()) + (Plane2Hits[i].Y() - Plane2Hits[j].Y()) * (Plane2Hits[i].Y() - Plane2Hits[j].Y()));
-            nPairs++;
+    for (Int_t iPlane = 0; iPlane < nPlanes; ++iPlane) {
+        Int_t nPairs = 0;
+        for (Int_t i = 0; i < fNTracks; ++i) {
+            TVector3 vI = PlaneHits[iPlane].at(i);
+            for (Int_t j = i + 1; j < fNTracks; ++j) {
+                TVector3 vJ = PlaneHits[iPlane].at(j);
+                dist[iPlane] += Sqrt(Sqr(vI.X() - vJ.X()) + Sqr(vI.Y() - vJ.Y()));
+                nPairs++;
+            }
         }
+        dist[iPlane] /= nPairs;
+    }
 
-    d0 /= nPairs;
-    d1 /= nPairs;
-    d2 /= nPairs;
+    Float_t minDist = FLT_MAX;
+    Float_t minZ = FLT_MAX;
+    for (Int_t iPlane = 0; iPlane < nPlanes; ++iPlane) {
+        if (dist[iPlane] < minDist) {
+            minZ = zPlane[iPlane];
+            minDist = dist[iPlane];
+        }
+    }
+    
+//    TCanvas* canv = new TCanvas("c", "c", 500, 500);
+//    TGraph *gr = new TGraph(nPlanes, zPlane, dist);
+//    gr->Draw("AP");
+//    canv->Update();
+//    getchar();
+//    delete gr;
+//    delete canv;
 
-    Float_t a = (d2 - (z2 * (d1 - d0) + z1 * d0 - z0 * d1) / (z1 - z0)) / (z2 * (z2 - z1 - z0) + z0 * z1);
-    Float_t b = (d1 - d0) / (z1 - z0) - a * (z0 + z1);
-
-    return (-0.5 * b / a);
+    return (minZ);
 }
 
 void BmnGemVertexFinder::FindVertexByVirtualPlanes() {

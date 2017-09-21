4d9841e409db44fea91389834fcfc2bac0e038b6
soul <soul@se51-99.jinr.ru>
2017-09-21 11:07:02 +0300

    temporary virtual machine with UniDB

--- uni_db/db_settings.h
+++ uni_db/db_settings.h
@@ -5,11 +5,11 @@
 const char* const UNI_DB_USERNAME = "db_reader";
 const char* const UNI_DB_PASSWORD = "reader_pass";
 const char* const UNI_DB_NAME = "bmn_db";               // for database adninistrator only
-const char* const UNI_DB_HOST = "nc13.jinr.ru";         // for database adninistrator only
+const char* const UNI_DB_HOST = "vm221-53.jinr.ru";     // for database adninistrator only
 
 const char* const TANGO_DB_USERNAME = "tango";
 const char* const TANGO_DB_PASSWORD = "tangompd";
 const char* const TANGO_DB_NAME = "hdbpp";              // for database adninistrator only
-const char* const TANGO_DB_HOST = "10.18.11.66";   	// for database adninistrator only
+const char* const TANGO_DB_HOST = "10.18.11.66";        // for database adninistrator only
 
 #endif // DB_SETTINGS_H

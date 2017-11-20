31f232cb4fa6a1ac7945a36fdca73d7094d01b5e
Anatoly Solomin <anatoly.solomin@jinr.ru>
2017-11-20 14:12:33 +0300

    Formal update of the globAlignment.C to address change of the BmnGlobalAlignment signature. Added the now standard root5/root6 feature. Minor code readability improvements

--- macro/alignment/globAlignment.C
+++ macro/alignment/globAlignment.C
@@ -1,9 +1,14 @@
 #include <TString.h>
 #include <TStopwatch.h>
 
-void globAlignment(UInt_t nEvents = 1e6, TString recoFileName = "bmndst.root",
+#include "../../gem/BmnGemStripConfiguration.h"
+#include "../run/bmnloadlibs.C"
+
+void globAlignment(UInt_t nEvents = 1e6, TString recoFileName = "bmndst.root", 
         TString addInfo = "") {
+#if ROOT_VERSION_CODE < ROOT_VERSION(5,99,99)
     gROOT->LoadMacro("$VMCWORKDIR/macro/run/bmnloadlibs.C");
+#endif
     bmnloadlibs(); // load BmnRoot libraries
     // -----   Timer   ---------------------------------------------------------
     TStopwatch timer;
@@ -28,11 +33,13 @@ void globAlignment(UInt_t nEvents = 1e6, TString recoFileName = "bmndst.root",
     // Restrictions on track params:
     //     globAlign->SetMinHitsAccepted(5);               // Default value is 3
     //     globAlign->SetChi2MaxPerNDF(2.);                // Cut on chi2/ndf for found tracks, default value is not limited
-    //    globAlign->SetTxMinMax(-0.05, 0.);
+    // globAlign->SetTxMinMax(-1., +1.);
     // globAlign->SetExclusionRangeTx(-0.02, 0.02);
     // globAlign->SetTyMinMax(-1., +1.);
     // globAlign->SetExclusionRangeTy(-0.02, 0.02);
-
+    globAlign->SetTxMinMax(-1.,   0.  );
+    globAlign->SetTyMinMax(-0.05, 0.05);
+    
     // Define modules to be fixed (any character) within alignment, if necessary. 
     // st0, st1, st2, st3_modLeft(0), st3_modRight(1), st4_modLeft(0), st4_modRight(1), st5_modLeft(0), st5_modRight(1)
     globAlign->SetGemFixedRun6("", "", "", "", "", "", "", "", "");
@@ -40,13 +47,16 @@ void globAlignment(UInt_t nEvents = 1e6, TString recoFileName = "bmndst.root",
     // A non-empty string means that the det. subsystem is used in the procedure
     // GEM tracker is included by default
     //                   "MWPC", "DCH"
-    globAlign->SetDetectors("", "");
-
+    globAlign->SetDetectors("",     "");
+  //globAlign->SetDetectors("MWPC", "DCH");
+  //globAlign->SetDetectors("MWPC", "");
+  //globAlign->SetDetectors("",     "DCH");
+    
     globAlign->SetPreSigma(0.001); // Default value is 1
     globAlign->SetAccuracy(0.001); // Default value is 0.001
     globAlign->SetUseRealHitErrors(kTRUE); // Default value is false
     // globAlign->SetUseTrackWithMinChi2(kTRUE);       // Default value is false
-    // globAlign->SetUseRegularization(kTRUE); // Default value is false
+    globAlign->SetUseRegularization(kTRUE); // Default value is false
     // globAlign->SetHugecut(200.);                    // Default value is 50
     // globAlign->SetChisqcut(5., 2.);                 // Default value is (0., 0.)
     // globAlign->SetEntriesPerParam(100);             // Default value is 10
@@ -54,7 +64,7 @@ void globAlignment(UInt_t nEvents = 1e6, TString recoFileName = "bmndst.root",
     globAlign->SetDwfractionCut(0.5); // Default value is 0, should be less than 0.5
 
     fRunAna->AddTask(globAlign);
-
+   
     fRunAna->Init();
     fRunAna->Run(0, nEvents);
     // -----   Finish   --------------------------------------------------------
@@ -65,6 +75,6 @@ void globAlignment(UInt_t nEvents = 1e6, TString recoFileName = "bmndst.root",
     cout << "Macro finished successfully." << endl;
     cout << "Real time " << rtime << " s, CPU time " << ctime << " s" << endl;
     cout << endl;
-
+    
     delete globAlign;
 }

a48717a2f89f5dcc6b9390ef5893470f11380f2a
George Laskaris <laskaris@jinr.ru>
2018-02-08 06:13:52 -0500

    Edited for current geo length

--- tof1/BmnTOF1Detector.cxx
+++ tof1/BmnTOF1Detector.cxx
@@ -69,6 +69,7 @@ BmnTOF1Detector::BmnTOF1Detector(Int_t NPlane, Int_t FillHist = 0) {
         Int_t istart = 49;
         if (fFillHist == 1) istart = 48;
         if (fFillHist == 2) istart = 0;
+	
         for (Int_t i = istart; i < fNStr + 1; i++) //
         {
             fName.Clear();
@@ -76,7 +77,7 @@ BmnTOF1Detector::BmnTOF1Detector(Int_t NPlane, Int_t FillHist = 0) {
             hTime[i] = new TH1I(fName, fName, 2000, 0, 1000);
             fHistListCh->Add(hTime[i]);
         }
-
+//std::cout << "Made it this far!\n";
         for (Int_t i = istart; i < fNStr + 1; i++) //
         {
             fName.Clear();
@@ -84,7 +85,7 @@ BmnTOF1Detector::BmnTOF1Detector(Int_t NPlane, Int_t FillHist = 0) {
             hDtLR[i] = new TH1I(fName, fName, 1024, -12., 12.);
             fHistListCh->Add(hDtLR[i]);
         }
-
+//std::cout << "Made it this far!\n";
         for (Int_t i = istart; i < fNStr + 1; i++) //
         {
             fName.Clear();
@@ -92,7 +93,7 @@ BmnTOF1Detector::BmnTOF1Detector(Int_t NPlane, Int_t FillHist = 0) {
             hWidth[i] = new TH1I(fName, fName, 1024, 12., 60.);
             fHistListCh->Add(hWidth[i]);
         }
-
+//std::cout << "Made it this far!\n";
         for (Int_t i = istart; i < fNStr + 1; i++) //
         {
             fName.Clear();
@@ -100,24 +101,28 @@ BmnTOF1Detector::BmnTOF1Detector(Int_t NPlane, Int_t FillHist = 0) {
             hDt[i] = new TH1I(fName, fName, 1024, -24., 24.);
             fHistListDt->Add(hDt[i]);
         }
-
+//std::cout << "Made it this far!\n";
         for (Int_t i = istart; i < fNStr + 1; i++) //
+      //for (Int_t i = istart; i < fNStr; i++)
         {
             fName.Clear();
             fName = Form("Hist_ToF_vs_AmpDet_%s_str%d", Name.Data(), i);
             hDtvsWidthDet[i] = new TH2S(fName, fName, 1024, 0., 48., 1024, -24., 24.);
             fHistListDt->Add(hDtvsWidthDet[i]);
         }
-
+//std::cout << "Made it this far!\n";
         for (Int_t i = istart; i < fNStr + 1; i++) //
+	//for (Int_t i = istart; i < fNStr; i++)
         {
+	    //cout << "In last loop, iteration " << i << "\n";
             fName.Clear();
             fName = Form("Hist_ToF_vs_AmpT0_%s_str%d", Name.Data(), i);
+	    //cout << fName << "\n";
             hDtvsWidthT0[i] = new TH2S(fName, fName, 1024, 0., 48., 1024, -24., 24.);
+	    //cout << hDtvsWidthT0[i] << "\n";
             fHistListDt->Add(hDtvsWidthT0[i]);
         }
-
-
+//std::cout << "Made it this far!\n";
     } else {
 
         hHitByCh = NULL;
@@ -349,7 +354,7 @@ Double_t BmnTOF1Detector::CalculateDt(Int_t Str = 0) {
             + 0.03428 * T0Amp * T0Amp
             - 0.0005853 * T0Amp * T0Amp * T0Amp);
 
-    if (gSlew[Str] != NULL) dt = dt - gSlew[Str]->Eval(fWidth[Str]) + 21.782; // 21.782 ns is ToF of light for 6.53 meters (Distance between TOF400 and T0)
+    if (gSlew[Str] != NULL) dt = dt - gSlew[Str]->Eval(fWidth[Str]) + 15.0; // 21.782 ns is ToF of light for 6.53 meters (Distance between TOF400 and T0) , 15.0 ns for 4.5 meters
 
     return dt;
 }
@@ -357,7 +362,7 @@ Double_t BmnTOF1Detector::CalculateDt(Int_t Str = 0) {
 //----------------------------------------------------------------------------------------
 
 TList* BmnTOF1Detector::GetList(Int_t n = 0) {
-    if (fFillHist == kTRUE) {
+    if (fFillHist >0) {
         if (n == 0) return fHistListStat;
         if (n == 1) return fHistListCh;
         if (n == 2) return fHistListDt;

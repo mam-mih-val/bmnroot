2aa2db446c540f17444978cc26c9f784c35cc61d
Sergei Merts <>
2018-08-24 09:34:52 +0300

    New tracking first release, DCH digitizer update, auxiliary files moved to math folder and small fixes

--- decoder/BmnDchRaw2Digit.cxx
+++ decoder/BmnDchRaw2Digit.cxx
@@ -1,14 +1,18 @@
 #include "BmnDchRaw2Digit.h"
 
 BmnDchRaw2Digit::BmnDchRaw2Digit(Int_t period, Int_t run) {
+    //ReadMapFromDB(period, run);
+    ReadMapFromFile(period);
+}
 
+BmnStatus BmnDchRaw2Digit::ReadMapFromDB(Int_t period, Int_t run) {
     fEntriesInMap1 = 0;
     fEntriesInMap2 = 0;
     fMap1 = NULL;
     fMap2 = NULL;
-    
+
     cout << "Loading the DCH Map from DB: Period " << period << ", Run " << run << "..." << endl;
-    
+
     UniDbDetectorParameter* pDetectorParameter1 = UniDbDetectorParameter::GetDetectorParameter("DCH1", "DCH_mapping", period, run);
     if (pDetectorParameter1 != NULL)
         pDetectorParameter1->GetDchMapArray(fMap1, fEntriesInMap1);
@@ -19,8 +23,81 @@ BmnDchRaw2Digit::BmnDchRaw2Digit(Int_t period, Int_t run) {
     delete pDetectorParameter2;
 }
 
+BmnStatus BmnDchRaw2Digit::ReadMapFromFile(Int_t period) {
+
+    fEntriesInMap1 = 120;
+    fEntriesInMap2 = 72; //72 for period 6, 120 for period 7
+
+    fMap1 = new DchMapStructure[fEntriesInMap1];
+    fMap2 = new DchMapStructure[fEntriesInMap2];
+    //    TString fileName = Form("DCH_map_Run%d.txt", period);
+    TString fileName = Form("DCH_map_Run%d_kirillov.txt", period);
+    TString path = TString(getenv("VMCWORKDIR")) + "/input/" + fileName;
+
+    TString dummy;
+    UInt_t ser = 0;
+    Int_t ch_l = 0;
+    Int_t ch_h = 0;
+    Int_t slot = 0;
+    Int_t group = 0;
+    TString name;
+
+    TString planes[16];
+    planes[0] = "VA_1";
+    planes[1] = "VB_1";
+    planes[2] = "UA_1";
+    planes[3] = "UB_1";
+    planes[4] = "YA_1";
+    planes[5] = "YB_1";
+    planes[6] = "XA_1";
+    planes[7] = "XB_1";
+    planes[8] = "VA_2";
+    planes[9] = "VB_2";
+    planes[10] = "UA_2";
+    planes[11] = "UB_2";
+    planes[12] = "YA_2";
+    planes[13] = "YB_2";
+    planes[14] = "XA_2";
+    planes[15] = "XB_2";
+
+    ifstream inFile(path.Data());
+    if (!inFile.is_open())
+        cout << "Error opening map-file (" << path << ")!" << endl;
+    inFile >> dummy >> dummy >> dummy >> dummy >> dummy >> dummy;
+    inFile >> dummy;
+    Int_t i1 = 0;
+    Int_t i2 = 0;
+    while (!inFile.eof()) {
+        inFile >> name >> group >> std::hex >> ser >> std::dec >> slot >> ch_l >> ch_h;
+        if (!inFile.good()) break;
+        for (Int_t planeId = 0; planeId < 16; ++planeId) {
+            if (name != planes[planeId]) continue;
+
+            //            printf("%s\t%d\t%d\t0x%x\t%d\t%d\t%d\n", name.Data(), planeId, group, ser, slot, ch_l, ch_h);
+
+            if (planeId / 8 == 0) {
+                fMap1[i1].plane = planeId;
+                fMap1[i1].group = group;
+                fMap1[i1].crate = ser;
+                fMap1[i1].slot = slot;
+                fMap1[i1].channel_low = ch_l;
+                fMap1[i1].channel_high = ch_h;
+                i1++;
+            } else {
+                fMap2[i2].plane = planeId;
+                fMap2[i2].group = group;
+                fMap2[i2].crate = ser;
+                fMap2[i2].slot = slot;
+                fMap2[i2].channel_low = ch_l;
+                fMap2[i2].channel_high = ch_h;
+                i2++;
+            }
+        }
+    }
+}
+
 void BmnDchRaw2Digit::FillEvent(TClonesArray *tdc, map<UInt_t, Long64_t> *ts, TClonesArray *dch, Double_t t0) {
-    
+
     for (Int_t i = 0; i < tdc->GetEntriesFast(); i++) {
         BmnTDCDigit *digit = (BmnTDCDigit*) tdc->At(i);
         if (digit->GetType() != DCH_TDC_TYPE) continue;
@@ -44,8 +121,8 @@ Int_t BmnDchRaw2Digit::GetChTDC64v(UInt_t tdc, UInt_t ch) {
 };
 
 BmnStatus BmnDchRaw2Digit::FindInMap(BmnTDCDigit* dig, TClonesArray* arr, Long64_t ts, Double_t t0, Int_t dchId) {
-    DchMapStructure* mapArr = (dchId == 1) ? fMap1 : fMap2; 
-    Int_t nEntriesInMap = (dchId == 1) ? fEntriesInMap1 : fEntriesInMap2; 
+    DchMapStructure* mapArr = (dchId == 1) ? fMap1 : fMap2;
+    Int_t nEntriesInMap = (dchId == 1) ? fEntriesInMap1 : fEntriesInMap2;
     for (Int_t iMap = 0; iMap < nEntriesInMap; ++iMap) {
         DchMapStructure map = mapArr[iMap];
         if (dig->GetSerial() != map.crate || dig->GetSlot() != map.slot) continue;

f6e1abbd03fe27358b276cc05b90f86a3d9c318d
Pavel Batyuk <pavel.batyuk@jinr.ru>
2018-10-12 15:33:09 +0300

    QA-tmp to be used when merging branches

--- QA/BmnClusteringQa.h
+++ QA/BmnClusteringQa.h
@@ -12,6 +12,8 @@
 #include "BmnDetectorSetup.h"
 #include <string>
 #include <vector>
+#include "BmnGemStripStationSet.h"
+#include "BmnSiliconStationSet.h"
 using std::string;
 using std::vector;
 using std::map;
@@ -189,6 +191,15 @@ private:
     //    TClonesArray* fGemClusterMatches;
     TClonesArray* fGemHitMatches;
 
+    BmnGemStripStationSet* fGemDetector;
+    BmnSiliconStationSet* fSilDetector;
+    
+    TClonesArray* fSilHits;
+    TClonesArray* fSilPoints;
+    TClonesArray* fSilHitMatches;
+    
+    
+   
     //    TClonesArray* fTof1Points;
     //    TClonesArray* fTof1Hits;
     //    TClonesArray* fTof1HitMatches;
@@ -197,10 +208,10 @@ private:
     //    TClonesArray* fTof2Hits;
     //    TClonesArray* fTof2HitMatches;
     //    
-    //    TClonesArray* fDch1Points;
-    //    TClonesArray* fDch1Hits;
-    //    TClonesArray* fDch1HitMatches;
-    //    
+    TClonesArray* fDch1Points;
+    TClonesArray* fDch1Hits;
+    TClonesArray* fDch1HitMatches;
+       
     //    TClonesArray* fDch2Points;
     //    TClonesArray* fDch2Hits;
     //    TClonesArray* fDch2HitMatches;
---
+++ QA/BmnLambdaQaReport.h
@@ -0,0 +1,79 @@
+/**
+ * \file BmnLambdaQaReport.h
+ * \brief visualising simulated lambda reconstruction performance calculation.
+ * \author Andrey Lebedev <andrey.lebedev@gsi.de> - original author for CBM experiment
+ * \author Sergey Merts <sergey.merts@gmail.com> - modifications for BMN experiment
+ * \author Alexander Lytaev <sas-lyt@ya.ru> - modifications for BMN experiment 
+ * \date 2018 July
+ */
+
+#ifndef BMNLAMBDAQAREPORT_H_
+#define BMNLAMBDAQAREPORT_H_
+
+#include "report/BmnSimulationReport.h"
+#include "BmnGemStripStationSet.h"
+#include "BmnGemStripStationSet_RunSpring2017.h"
+#include <TClonesArray.h>
+#include <string>
+using std::string;
+
+class BmnLambdaQaReport : public BmnSimulationReport {
+public:
+    /**
+     * \brief Constructor.
+     */
+    BmnLambdaQaReport(Bool_t fUseMCFile = kTRUE, Bool_t fUseRecoFile = kFALSE, Short_t key2 = 'a', TString name = "lambda_qa", TString keyAddition = "", Bool_t drawPoints = kTRUE, TClonesArray* particlePairsInfo = NULL);
+
+    /**
+     * \brief Destructor.
+     */
+    virtual ~BmnLambdaQaReport();
+
+private:
+
+    virtual void Create();
+    virtual void Draw();
+    void PrintEventInfo(); 
+    void DrawNumberOfReconstructableLambdaHistograms(const string& canvasName);
+    void DrawNumberOfLambdaHistograms(const string& canvasName);
+    void DrawGeomEfficiencyHistograms(const string& canvasName);
+    void DrawTwoDimensionalRecGeomEfficiencyHistograms(const string& canvasName);
+    void DrawNumberOfLambdaDecayProtonsHistograms( const string& canvasName );
+    void DrawTwoDimensionalRecProtonsRecEfficiencyHistograms(const string& canvasName);
+    void DrawRecProtonsGeomEffHistograms( const string& canvasName );
+    void DrawNumberOfLambdaDecayMesonsHistograms( const string& canvasName );
+    void DrawRecMesonsGeomRecEffHistograms( const string& canvasName );
+    void DrawTwoDimensionalRecMesonsGeomRecEfficiencyHistograms(const string& canvasName);
+    void DrawNumberOfReconstructedLambdaHistograms(const string& canvasName);
+    void DrawReconstructionEfficiencyHistograms(const string& canvasName);
+    void DrawTwoDimensionalRealRecEfficiencyHistograms( const string& canvasName );
+    void DrawNumberOfLambdaDecayProtonsReconstructedHistograms( const string& canvasName );
+    void DrawTwoDimensionalRealRecEfficiencyRecProtonsHistograms( const string& canvasName );
+    void DrawRecProtonsReconstructionEfficiencyHistograms(const string& canvasName);
+    void DrawNumberOfLambdaDecayMesonsReconstructedHistograms( const string& canvasName );
+    void DrawTwoDimensionalRealRecEfficiencyRecMesonsHistograms( const string& canvasName );
+    void DrawRecMesonsReconstructionEfficiencyHistograms(const string& canvasName);
+    void DrawNumberOfNotReconstructedLambdasHistograms(const string& canvasName);
+    void DrawReconstructedFromMCLambdasHistograms(const string& canvasName);
+    void DrawTwoDimensinalReconstructedFromMCLambdasHistograms(const string& canvasName);
+    void DrawReconstructedFromMCWOCutsLambdasHistograms(const string& canvasName);
+    void DrawTwoDimensinalReconstructedFromMCWOCutsLambdasHistograms(const string& canvasName);
+    void DrawReconstructedLambdasHistograms(const string& canvasName);
+    void DrawTwoDimensinalReconstructedLambdasHistograms(const string& canvasName);
+    void DrawReconstructedLambdasWOCutsHistograms(const string& canvasName);
+    void DrawTwoDimensinalReconstructedLambdasWOCutsHistograms(const string& canvasName);
+       
+    TString fOutName;
+    Bool_t fUseMCFile;
+    Bool_t fUseRecoFile;
+    Bool_t fDrawPoints;
+    Char_t fKey2;
+    TString fKeyAddition;
+    TString drawPointsOpt;
+    TClonesArray* fParticlePairsInfoMC;
+    TClonesArray* fParticlePairsInfoReco;
+    
+    ClassDef(BmnLambdaQaReport, 1)
+};
+
+#endif /* BMNLAMBDAQAREPORT_H_ */

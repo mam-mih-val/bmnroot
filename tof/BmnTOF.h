aac0f0168df660d6d044eb21f74d4a31f3498494
Konstantin Gertsenberger <soul@se51-99.jinr.ru>
2018-12-05 17:22:27 +0300

    correct bug: operations with NaN; align code lines for comfort viewing

--- tof/BmnTOF.h
+++ tof/BmnTOF.h
@@ -1,70 +1,62 @@
-//--------------------------------------------------------------------------------------------------------------------------------------
 #ifndef __BMNTOF_H
 #define __BMNTOF_H 1
 
+#include "BmnTOFPoint.h"
+
+#include "FairDetector.h"
+#include "FairVolume.h"
 
 #include "TClonesArray.h"
 #include "TVector3.h"
 #include "TLorentzVector.h"
-#include "FairDetector.h"
-#include "BmnTOFGeoPar.h"
 
-using namespace std;
+#include <string>
 
-
-class TClonesArray;
-class BmnTOFPoint;
-class FairVolume;
-
-//--------------------------------------------------------------------------------------------------------------------------------------
 class BmnTOF : public FairDetector
 {
-public:
-	BmnTOF();
-	BmnTOF(const char* name, Bool_t active);
-	virtual ~BmnTOF();
+ public:
+    BmnTOF();
+    BmnTOF(const char* name, Bool_t active);
+    virtual ~BmnTOF();
 
-	virtual void Initialize();
-	virtual Bool_t ProcessHits(FairVolume* vol = nullptr);
-	virtual void EndOfEvent();
-	virtual void BeginEvent();
-	virtual void Register();
-	virtual TClonesArray* GetCollection(Int_t iColl) const;
-	virtual void Print() const;    
-	virtual void Reset();
-	virtual void CopyClones(TClonesArray* cl1, TClonesArray* cl2, Int_t offset);
-	virtual void ConstructGeometry();
-	virtual void ConstructAsciiGeometry();
- 	virtual Bool_t CheckIfSensitive(std::string name);
+    virtual void Initialize();
+    virtual Bool_t ProcessHits(FairVolume* vol = nullptr);
+    virtual void EndOfEvent();
+    virtual void BeginEvent();
+    virtual void Register();
+    virtual TClonesArray* GetCollection(Int_t iColl) const;
+    virtual void Print() const;
+    virtual void Reset();
+    virtual void CopyClones(TClonesArray* cl1, TClonesArray* cl2, Int_t offset);
+    virtual void ConstructGeometry();
+    virtual void ConstructAsciiGeometry();
+    virtual Bool_t CheckIfSensitive(std::string name);
 
-	BmnTOFPoint* AddPoint(Int_t trackID, Int_t detID, TVector3 pos, TVector3 mom, Double_t tof, Double_t length, Double_t eLoss);
+    BmnTOFPoint* AddPoint(Int_t trackID, Int_t detID, TVector3 pos, TVector3 mom, Double_t tof, Double_t length, Double_t eLoss);
 
-private:
-	TLorentzVector 	fPos;               //!  position
-	TLorentzVector 	fMom;               //!  momentum
-	Double32_t     	fTime;              //!  time
-	Double32_t     	fLength;            //!  length
-	Double32_t     	fELoss;             //!  energy loss
-	Int_t 		fPosIndex;		//! 
-  	const double	nan;			//!
+ private:
+    TLorentzVector 	fPos;               //! position
+    TLorentzVector 	fMom;               //! momentum
+    Double32_t     	fTime;              //! time
+    Double32_t     	fLength;            //! length
+    Double32_t     	fELoss;             //! energy loss
+    Int_t 		    fPosIndex;		    //!
+    const double	nan;			    //! KG: nan is set to -10 000 000, but i think it should be 0
   	
-  	TClonesArray	*fTofCollection;	//! Hit collection
+    TClonesArray	*fTofCollection;	//! Hit collection
   
-	// reset all parameters   
-	void ResetParameters();
+    // reset all parameters
+    void ResetParameters();
 
-ClassDef(BmnTOF, 3)
-}; 
-//--------------------------------------------------------------------------------------------------------------------------------------
+ ClassDef(BmnTOF, 3)
+};
+
 inline void BmnTOF::ResetParameters() 
 {
 	fPos.SetXYZM(nan, nan, nan, nan);
 	fMom.SetXYZM(nan, nan, nan, nan);
 	fTime = fLength = fELoss = nan;
 	fPosIndex = 0;
-};
-
+}
 
 #endif
-//--------------------------------------------------------------------------------------------------------------------------------------
-

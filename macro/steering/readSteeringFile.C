ad70036cf3cd9979bb82c08af63e1f45e0dd6f44
Pavel Batyuk <pavel.batyuk@jinr.ru>
2019-01-25 17:17:29 +0300

    Steering directory updated with a file valid for SRC

--- macro/steering/readSteeringFile.C
+++ macro/steering/readSteeringFile.C
@@ -1,14 +1,15 @@
 #include <Rtypes.h>
+
 R__ADD_INCLUDE_PATH($VMCWORKDIR)
 #include "macro/run/bmnloadlibs.C"
 
 R__LOAD_LIBRARY(libPhysics.so)
-        
-void readSteeringFile(TString file = "gemTrackingSteerCA.dat") {
-     bmnloadlibs(); // load BmnRoot libraries
-     
-     BmnSteeringCA* steer = new BmnSteeringCA(file);
-     steer->PrintParamTable();
-     
-     delete steer;
+
+void readSteeringFile(TString file = "") {
+    bmnloadlibs(); // load BmnRoot libraries
+
+    BmnSteering* steer = new BmnSteering(file);
+    steer->PrintParamTable();
+
+    delete steer;
 }

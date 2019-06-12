ccada914896cf89f836dd0ee8484dfc7e18da300
Pavel Batyuk <pavel.batyuk@jinr.ru>
2019-06-12 19:18:41 +0300

    Digi-merge task updated to be used for BM@N and SRC

--- miscellaneous/LinkDef.h
+++ miscellaneous/LinkDef.h
@@ -4,11 +4,6 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class BmnDigiConverter++;
-#pragma link C++ class Cbm2BmnHitConverter++;
-// #pragma link C++ class BmnQaOffline++;
-// #pragma link C++ class BmnGemDigiQa++;
-// #pragma link C++ class BmnSiliconDigiQa++;
 #pragma link C++ class BmnDigiContainer++;
 #pragma link C++ class BmnDigiMergeTask++;
 

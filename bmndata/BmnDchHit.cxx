0d59943e7ebcb9d6ea09a67fecddf47fbb36a70f
dbaranov <dmitriybaranof@gmail.com>
2019-09-11 16:25:04 +0300

    DCH: previous class 'BmnDchHitProducer' has been replaced with a new class

--- bmndata/BmnDchHit.cxx
+++ bmndata/BmnDchHit.cxx
@@ -63,14 +63,14 @@ void BmnDchHit::Print(const Option_t* opt) const {
     cout << "    Flag: " << fFlag << endl;
 }
 //------------------------------------------------------------------------------------------------------------------------
-
+/*
 void BmnDchHit::SetIndex(Int_t indx) {
     /// Add point index
 
     Int_t size = fIndex.GetSize();
     fIndex.Set(size + 1);
     fIndex[size] = indx;
-}
+}*/
 //------------------------------------------------------------------------------------------------------------------------
 
 Int_t BmnDchHit::Compare(const TObject* hit) const {

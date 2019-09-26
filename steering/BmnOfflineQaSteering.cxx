f0f1ad153110c06a52bb27a5d584d4b482e2188d
Pavel Batyuk <pavel.batyuk@jinr.ru>
2019-09-26 20:21:07 +0300

    Added compatibility of the QA-system to RUN6,7 (BM@N and SRC)

--- steering/BmnOfflineQaSteering.cxx
+++ steering/BmnOfflineQaSteering.cxx
@@ -44,15 +44,40 @@ void BmnOfflineQaSteering::ParseSteerFile(TString fileName) {
             fCanvNames.push_back(det);
         }
     }
-    
+
     // Get canvas ordering
     for (Int_t iDet = 0; iDet < fNdets; iDet++) {
-       getline(f, line); 
-       TString detBuff;
-       Int_t pad1, pad2;
-       f >> detBuff >> pad1 >> pad2;
-       fCanvDetCorresp[detBuff] = pair <Int_t, Int_t> (pad1, pad2);  
+        getline(f, line);
+        TString detBuff;
+        Int_t pad1, pad2;
+        f >> detBuff >> pad1 >> pad2;
+        fCanvDetCorresp[detBuff] = pair <Int_t, Int_t> (pad1, pad2);
+    }
+
+    // Get period, exp. setup, start and finish run id, set of detectors and triggers ...
+    TString buff = "";
+    for (Int_t iLine = 0; iLine < 3; iLine++) {
+        getline(f, line);
+        Int_t period, start, finish, Ndets, Ntrigs;
+        TString setup, detector, trigger;
+        f >> period >> setup >> start >> finish >> buff >> Ndets;
+        fBorderRuns[make_pair(period, setup)] = make_pair(start, finish);
+        
+        vector <TString> detectors;
+        
+        for (Int_t iDet = 0; iDet < Ndets; iDet++) {
+            f >> detector;
+            detectors.push_back(detector);       
+        }
+        fDetectors[make_pair(period, setup)] = detectors;
+        
+        vector <TString> triggers;
+        f >> buff >> Ntrigs;
+        for (Int_t iTrigger = 0; iTrigger < Ntrigs; iTrigger++) { 
+            f >> trigger;
+            triggers.push_back(trigger);            
+        }
+        fTriggers[make_pair(period, setup)] = triggers;
     }
 }
-
 
--- steering/BmnOfflineQaSteering.h
+++ steering/BmnOfflineQaSteering.h
@@ -24,13 +24,65 @@ public:
     Int_t*** GetDetCanvas() {
         return fDetCanvas;
     }
-    
+
     vector <TString> GetCanvNames() {
         return fCanvNames;
     }
-    
+
     map <TString, pair <Int_t, Int_t>> GetCorrMap() {
         return fCanvDetCorresp;
+    }
+
+    pair <Int_t, Int_t> GetBorderRuns(Int_t period, TString setup = "BM@N") {
+        if (period == 6 && setup == "SRC")
+            return make_pair(-1, -1);
+        return fBorderRuns.find(make_pair(period, setup))->second;
+    }
+
+    vector <TString> GetDetectors(Int_t period, TString setup = "BM@N") {
+        return fDetectors.find(make_pair(period, setup))->second;
+    }
+
+    vector <TString> GetTriggers(Int_t period, TString setup = "BM@N") {
+        return fTriggers.find(make_pair(period, setup))->second;
+    }
+
+    pair <Int_t, TString> GetRunAndSetupByRunId(Int_t id) {
+        const Int_t nPeriods = 2;
+        const Int_t nSetups = 2;
+
+        Int_t runPeriods[nPeriods] = {6, 7};
+        TString setups[nSetups] = {"BM@N", "SRC"};
+
+        for (Int_t iPeriod = 0; iPeriod < nPeriods; iPeriod++)
+            for (Int_t iSetup = 0; iSetup < nSetups; iSetup++)
+                if (id >= GetBorderRuns(runPeriods[iPeriod], setups[iSetup]).first && id <= GetBorderRuns(runPeriods[iPeriod], setups[iSetup]).second)
+                    return make_pair(runPeriods[iPeriod], setups[iSetup]);
+
+        return make_pair(-1, "");
+    }
+
+    Int_t GetNumberOfDets(Int_t period, TString setup, TString type) {
+        // Possible det. types are: coordinate, time, calorimeter
+        if (type != "coordinate" && type != "time" && type != "calorimeter")
+            return -1;
+        Int_t detCounter = 0;
+        for (Int_t iDet = 0; iDet < GetDetectors(period, setup).size(); iDet++) {
+            TString det = GetDetectors(period, setup)[iDet];
+
+            if (type == "coordinate") {
+                if (det.Contains("GEM") || det.Contains("SILICON") || det.Contains("CSC"))
+                    detCounter++;
+            } else if (type == "time") {
+                if (det.Contains("TOF400") || det.Contains("TOF700") || det.Contains("MWPC") || det.Contains("DCH"))
+                    detCounter++;
+            }
+            else if (type == "calorimeter") {
+                if (det.Contains("ECAL") || det.Contains("ZDC"))
+                    detCounter++;
+            }
+        }
+        return detCounter;
     }
 
 private:
@@ -50,10 +102,14 @@ private:
 
     Int_t fNdets;
     Int_t*** fDetCanvas;
-        
+
     vector <TString> fCanvNames;
-    
+
     map <TString, pair <Int_t, Int_t>> fCanvDetCorresp;
+
+    map <pair <Int_t, TString>, pair <Int_t, Int_t>> fBorderRuns; // (period, setup) --> (start, finish)
+    map <pair <Int_t, TString>, vector <TString>> fDetectors; // (period, setup) --> list of detectors
+    map <pair <Int_t, TString>, vector <TString>> fTriggers; // (period, setup) --> list of triggers
 
     ClassDef(BmnOfflineQaSteering, 1);
 };

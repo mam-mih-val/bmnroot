994432896123ebcbc443f151388bf9cb9270e185
Pavel Batyuk <pavel.batyuk@jinr.ru>
2019-09-26 20:23:52 +0300

    Added compatibility of the QA-system to RUN6,7 (BM@N and SRC)

--- QA/offline/BmnQaOffline.cxx
+++ QA/offline/BmnQaOffline.cxx
@@ -3,81 +3,16 @@
 Int_t BmnQaOffline::fCurrentEvent = 0;
 
 BmnQaOffline::BmnQaOffline(TString file) :
-fGemDigits(nullptr),
-fCscDigits(nullptr),
-fSiDigits(nullptr),
-fTOF400Digits(nullptr),
-fDchDigits(nullptr),
-fMwpcDigits(nullptr),
-fTOF700Digits(nullptr),
-fECALDigits(nullptr),
-fZDCDigits(nullptr),
-fBC1Digits(nullptr),
-fBC2Digits(nullptr),
-fVetoDigits(nullptr),
-fBC3Digits(nullptr),
-fBC4Digits(nullptr),
-fX1LDigits(nullptr),
-fX2LDigits(nullptr),
-fY1LDigits(nullptr),
-fY2LDigits(nullptr),
-fX1RDigits(nullptr),
-fX2RDigits(nullptr),
-fY1RDigits(nullptr),
-fY2RDigits(nullptr),
-fTQDC_BC1Digits(nullptr),
-fTQDC_BC2Digits(nullptr),
-fTQDC_BC3Digits(nullptr),
-fTQDC_BC4Digits(nullptr),
-fTQDC_VetoDigits(nullptr),
-fSiTrigDigits(nullptr),
-fBDDigits(nullptr),
 fBmnHeader(nullptr),
-fDirectories(nullptr),
-fDetectors(nullptr),
-fTriggers(nullptr),
 fChainDst(nullptr),
 fSiliconHits(nullptr),
 fSiliconTracks(nullptr),
 fGemHits(nullptr),
 fGemTracks(nullptr),
 fVertex(nullptr),
-fGlobalTracks(nullptr) {
-    fGem = "GEM";
-    fCsc = "CSC";
-    fSi = "SILICON";
-    fTOF400 = "TOF400";
-    fDch = "DCH";
-    fMwpc = "MWPC";
-    fTOF700 = "TOF700";
-    fECAL = "ECAL";
-    fZDC = "ZDC";
-
-    fBC1 = "BC1";
-    fBC2 = "BC2";
-    fVeto = "VETO";
-
-    fBC3 = "BC3";
-    fBC4 = "BC4";
-    fX1L = "X1L";
-    fX2L = "X2L";
-    fY1L = "Y1L";
-    fY2L = "Y2L";
-    fX1R = "X1R";
-    fX2R = "X2R";
-    fY1R = "Y1R";
-    fY2R = "Y2R";
-
-    fTQDC_BC1 = "TQDC_BC1";
-    fTQDC_BC2 = "TQDC_BC2";
-    fTQDC_BC3 = "TQDC_BC3";
-    fTQDC_BC4 = "TQDC_BC4";
-    fTQDC_Veto = "TQDC_VC";
-
-    fSiTrig = "Si";
-    fBD = "BD";
-
-    isDstRead = ReadDstTree(file);
+fGlobalTracks(nullptr),
+fSteering(new BmnOfflineQaSteering()) {
+      isDstRead = ReadDstTree(file);
 }
 
 Bool_t BmnQaOffline::ReadDstTree(TString fileDst) {
@@ -110,75 +45,40 @@ InitStatus BmnQaOffline::Init() {
     ioman = FairRootManager::Instance();
 
     fBmnHeader = (BmnEventHeader*) ioman->GetObject("BmnEventHeader.");
+    // Get general info on period and exp. setup, detectors and trigger been
+    period = fSteering->GetRunAndSetupByRunId(fBmnHeader->GetRunId()).first;
+    setup = fSteering->GetRunAndSetupByRunId(fBmnHeader->GetRunId()).second;
+    nDets = fSteering->GetDetectors(period, setup).size(); // Number of detectors should be set for a certain run / setup extension
+    Int_t nTrigs = fSteering->GetTriggers(period, setup).size(); // Number of triggers should be set for a certain run / setup extension
 
-    fGemDigits = (TClonesArray*) ioman->GetObject(fGem.Data());
-    fCscDigits = (TClonesArray*) ioman->GetObject(fCsc.Data());
-    fSiDigits = (TClonesArray*) ioman->GetObject(fSi.Data());
-    fTOF400Digits = (TClonesArray*) ioman->GetObject(fTOF400.Data());
-    fDchDigits = (TClonesArray*) ioman->GetObject(fDch.Data());
-    fMwpcDigits = (TClonesArray*) ioman->GetObject(fMwpc.Data());
-    fTOF700Digits = (TClonesArray*) ioman->GetObject(fTOF700.Data());
-    fECALDigits = (TClonesArray*) ioman->GetObject(fECAL.Data());
-    fZDCDigits = (TClonesArray*) ioman->GetObject(fZDC.Data());
+    // Read input arrays with det. and trig. info ...
+    DETECTORS = new TClonesArray*[nDets];
+    TRIGGERS = new TClonesArray*[nTrigs];
 
-    fBC1Digits = (TClonesArray*) ioman->GetObject(fBC1.Data());
-    fBC2Digits = (TClonesArray*) ioman->GetObject(fBC2.Data());
-    fVetoDigits = (TClonesArray*) ioman->GetObject(fVeto.Data());
+    for (Int_t iDet = 0; iDet < nDets; iDet++)
+        DETECTORS[iDet] = (TClonesArray*) ioman->GetObject(fSteering->GetDetectors(period, setup)[iDet].Data());
 
-    fBC3Digits = (TClonesArray*) ioman->GetObject(fBC3.Data());
-    fBC4Digits = (TClonesArray*) ioman->GetObject(fBC4.Data());
-    fX1LDigits = (TClonesArray*) ioman->GetObject(fX1L.Data());
-    fX2LDigits = (TClonesArray*) ioman->GetObject(fX2L.Data());
-    fY1LDigits = (TClonesArray*) ioman->GetObject(fY1L.Data());
-    fY2LDigits = (TClonesArray*) ioman->GetObject(fY2L.Data());
-    fX1RDigits = (TClonesArray*) ioman->GetObject(fX1R.Data());
-    fX2RDigits = (TClonesArray*) ioman->GetObject(fX2R.Data());
-    fY1RDigits = (TClonesArray*) ioman->GetObject(fY1R.Data());
-    fY2RDigits = (TClonesArray*) ioman->GetObject(fY2R.Data());
+    for (Int_t iTrigger = 0; iTrigger < nTrigs; iTrigger++)
+        TRIGGERS[iTrigger] = (TClonesArray*) ioman->GetObject(fSteering->GetTriggers(period, setup)[iTrigger].Data());
 
-    fTQDC_BC1Digits = (TClonesArray*) ioman->GetObject(fTQDC_BC1.Data());
-    fTQDC_BC2Digits = (TClonesArray*) ioman->GetObject(fTQDC_BC2.Data());
-    fTQDC_BC3Digits = (TClonesArray*) ioman->GetObject(fTQDC_BC3.Data());
-    fTQDC_BC4Digits = (TClonesArray*) ioman->GetObject(fTQDC_BC4.Data());
-    fTQDC_VetoDigits = (TClonesArray*) ioman->GetObject(fTQDC_Veto.Data());
+    nCoordinate = fSteering->GetNumberOfDets(period, setup, "coordinate");
+    nTime = fSteering->GetNumberOfDets(period, setup, "time");
+    nCalorimeter = fSteering->GetNumberOfDets(period, setup, "calorimeter");
 
-    fSiTrigDigits = (TClonesArray*) ioman->GetObject(fSiTrig.Data());
-    fBDDigits = (TClonesArray*) ioman->GetObject(fBD.Data());
+    coordinate = new BmnCoordinateDetQa*[nCoordinate];
+    for (Int_t iDet = 0; iDet < nCoordinate; iDet++)
+        coordinate[iDet] = new BmnCoordinateDetQa(fSteering->GetDetectors(period, setup)[iDet], fBmnHeader->GetRunId());
 
-    // Call detector classes and its histo classes implicitly
-    // Strip detectors
-    gem = new BmnCoordinateDetQa("GEM", fBmnHeader->GetRunId());
-    silicon = new BmnCoordinateDetQa("SILICON", fBmnHeader->GetRunId());
-    csc = new BmnCoordinateDetQa("CSC", fBmnHeader->GetRunId());
+    time = new BmnTimeDetQa*[nTime];
+    for (Int_t iDet = nCoordinate; iDet < nCoordinate + nTime; iDet++)
+        time[iDet - nCoordinate] = new BmnTimeDetQa(fSteering->GetDetectors(period, setup)[iDet]);
 
-    // Time detectors
-    tof400 = new BmnTimeDetQa("TOF400");
-    tof700 = new BmnTimeDetQa("TOF700");
-    dch = new BmnTimeDetQa("DCH");
-    mwpc = new BmnTimeDetQa("MWPC");
+    calorimeter = new BmnCalorimeterDetQa*[nCalorimeter];
+    for (Int_t iDet = nCoordinate + nTime; iDet < nCoordinate + nTime + nCalorimeter; iDet++)
+        calorimeter[iDet - nCoordinate - nTime] = new BmnCalorimeterDetQa(fSteering->GetDetectors(period, setup)[iDet]);
 
-    // Calorim. detectors
-    ecal = new BmnCalorimeterDetQa("ECAL");
-    zdc = new BmnCalorimeterDetQa("ZDC");
-
-    // Triggers
-    const Int_t nTrigDets = fBmnHeader->GetRunId() > 3589 ? 6 : 18; // FIXME (BM@N or SRC)
-    TString trigsBMN[] = {"BC1", "BC2", "BC3", "VETO", "SI", "BD"};
-    TClonesArray * _trigsBMN[] = {fBC1Digits, fBC2Digits, fBC3Digits, fVetoDigits, fSiTrigDigits, fBDDigits};
-
-    TString trigsSRC[] = {"BC1", "BC2", "BC3", "BC4", "X1L", "X1R", "X2L", "X2R", "Y1L", "Y1R", "Y2L", "Y2R", "VC", "TQDC_BC1", "TQDC_BC2", "TQDC_BC3", "TQDC_BC4", "TQDC_VC"};
-    TClonesArray * _trigsSRC[] = {fBC1Digits, fBC2Digits, fBC3Digits, fBC4Digits,
-        fX1LDigits, fX1RDigits, fX2LDigits, fX2RDigits,
-        fY1LDigits, fY1RDigits, fY2LDigits, fY2RDigits,
-        fVetoDigits,
-        fTQDC_BC1Digits, fTQDC_BC2Digits, fTQDC_BC3Digits, fTQDC_BC4Digits, fTQDC_VetoDigits};
-
-    fTriggers = new TClonesArray*[nTrigDets];
-
-    for (Int_t iDet = 0; iDet < nTrigDets; iDet++) {
-        fTriggers[iDet] = (nTrigDets == 6) ? _trigsBMN[iDet] : _trigsSRC[iDet];
-        fTrigCorr[fTriggers[iDet]] = (nTrigDets == 6) ? trigsBMN[iDet] : trigsSRC[iDet];
-    }
+    for (Int_t iDet = 0; iDet < nTrigs; iDet++)
+        fTrigCorr[TRIGGERS[iDet]] = fSteering->GetTriggers(period, setup)[iDet];
 
     if (fTrigCorr.size() != 0)
         triggers = new BmnTrigDetQa(fTrigCorr);
@@ -190,31 +90,30 @@ InitStatus BmnQaOffline::Init() {
 }
 
 void BmnQaOffline::Finish() {
-    const Int_t nDets = 10;
+    vector <TString> detectors;
 
-    fDetectors = new TString[nDets];
-    fDetectors[0] = "GEM";
-    fDetectors[1] = "SILICON";
-    fDetectors[2] = "CSC";
+    // TRIGGERS are considered as a whole detector when saving to output file
+    for (Int_t iDet = 0; iDet < nDets; iDet++)
+        detectors.push_back(fSteering->GetDetectors(period, setup)[iDet]);
+    detectors.push_back("TRIGGERS");
 
-    fDetectors[3] = "TOF400";
-    fDetectors[4] = "TOF700";
-    fDetectors[5] = "DCH";
-    fDetectors[6] = "MWPC";
+    // Fill a vector with histo managers
+    vector <BmnQaHistoManager*> managers;
+    for (Int_t iDet = 0; iDet < nCoordinate; iDet++)
+        managers.push_back(coordinate[iDet]->GetManager());
 
-    fDetectors[7] = "ECAL";
-    fDetectors[8] = "ZDC";
+    for (Int_t iDet = 0; iDet < nTime; iDet++)
+        managers.push_back(time[iDet]->GetManager());
 
-    fDetectors[9] = "TRIGGERS";
+    for (Int_t iDet = 0; iDet < nCalorimeter; iDet++)
+        managers.push_back(calorimeter[iDet]->GetManager());
+    
+    managers.push_back(triggers->GetManager());
 
-    BmnQaHistoManager * managers[nDets] = {gem->GetManager(), silicon->GetManager(), csc->GetManager(),
-        tof400->GetManager(), tof700->GetManager(), dch->GetManager(), mwpc->GetManager(),
-        ecal->GetManager(), zdc->GetManager(), triggers->GetManager()};
-
-    fDirectories = new TDirectory*[nDets];
-    for (Int_t iDet = 0; iDet < nDets; iDet++) {
-        fDirectories[iDet] = ioman->GetOutFile()->mkdir(fDetectors[iDet].Data());
-        fDirectories[iDet]->cd();
+    TDirectory** directories = new TDirectory*[nDets + 1];
+    for (Int_t iDet = 0; iDet < nDets + 1; iDet++) {
+        directories[iDet] = ioman->GetOutFile()->mkdir(detectors[iDet].Data());
+        directories[iDet]->cd();
         managers[iDet]->WriteToFile();
     }
 
@@ -222,19 +121,17 @@ void BmnQaOffline::Finish() {
     dst->GetManager()->WriteToFile();
 
     // Delete detector classes and its histo classes
-    delete gem;
-    delete silicon;
-    delete csc;
-    delete tof400;
-    delete tof700;
-    delete dch;
-    delete mwpc;
-    delete ecal;
-    delete zdc;
+    for (Int_t iDet = 0; iDet < nCoordinate; iDet++)
+        delete coordinate[iDet];
+    
+    for (Int_t iDet = 0; iDet < nTime; iDet++)
+        delete time[iDet];
+    
+    for (Int_t iDet = 0; iDet < nCalorimeter; iDet++)
+        delete calorimeter[iDet];
+    
     delete triggers;
     delete dst;
-
-    delete fTriggers;
 }
 
 void BmnQaOffline::Exec(Option_t* opt) {
@@ -249,24 +146,54 @@ void BmnQaOffline::Exec(Option_t* opt) {
         cout << "Event# = " << fCurrentEvent << endl;
 
     // Coord. dets
-    GetDistributionOfFiredStrips <BmnGemStripDigit> (fGemDigits, gem, "GEM"); // histos 1
-    GetDistributionOfFiredStripsVsSignal <BmnGemStripDigit> (fGemDigits, gem, "GEM"); // histos 2
-    GetDistributionOfFiredStrips <BmnSiliconDigit> (fSiDigits, silicon, "SILICON");
-    GetDistributionOfFiredStrips <BmnCSCDigit> (fCscDigits, csc, "CSC");
+    for (Int_t iDet = 0; iDet < nCoordinate; iDet++) {
+        TString detName = fSteering->GetDetectors(period, setup)[iDet];
+        TClonesArray* detData = DETECTORS[iDet];
+        BmnCoordinateDetQa* detQa = coordinate[iDet];
+
+        if (detName.Contains("GEM")) {
+            GetDistributionOfFiredStrips <BmnGemStripDigit> (detData, detQa, detName); // histos 1
+            GetDistributionOfFiredStripsVsSignal <BmnGemStripDigit> (detData, detQa, detName); // histos 2
+        } else if (detName.Contains("SILICON")) {
+            GetDistributionOfFiredStrips <BmnSiliconDigit> (detData, detQa, detName);
+        } else if (detName.Contains("CSC")) {
+            GetDistributionOfFiredStrips <BmnCSCDigit> (detData, detQa, detName);
+        }
+    }
 
     // Time dets
-    GetCommonInfo <BmnTof1Digit> (fTOF400Digits, tof400, "TOF400");
-    GetTofInfo <BmnTof1Digit> (fTOF400Digits, tof400, "TOF400");
-    GetCommonInfo <BmnTof2Digit> (fTOF700Digits, tof700, "TOF700");
-    GetTofInfo <BmnTof2Digit> (fTOF700Digits, tof700, "TOF700");
-    GetCommonInfo <BmnDchDigit> (fDchDigits, dch, "DCH");
-    GetMwpcDchInfo <BmnDchDigit> (fDchDigits, dch, "DCH");
-    GetCommonInfo <BmnMwpcDigit> (fMwpcDigits, mwpc, "MWPC");
-    GetMwpcDchInfo <BmnMwpcDigit> (fMwpcDigits, mwpc, "MWPC");
+    for (Int_t iDet = nCoordinate; iDet < nCoordinate + nTime; iDet++) {
+        TString detName = fSteering->GetDetectors(period, setup)[iDet];
+        TClonesArray* detData = DETECTORS[iDet];
+        BmnTimeDetQa* detQa = time[iDet - nCoordinate];
+
+        if (detName.Contains("TOF400")) {
+            GetCommonInfo <BmnTof1Digit> (detData, detQa, detName);
+            GetTofInfo <BmnTof1Digit> (detData, detQa, detName);
+        } else if (detName.Contains("TOF700")) {
+            GetCommonInfo <BmnTof2Digit> (detData, detQa, detName);
+            GetTofInfo <BmnTof2Digit> (detData, detQa, detName);
+        } else if (detName.Contains("DCH")) {
+            GetCommonInfo <BmnDchDigit> (detData, detQa, detName);
+            GetMwpcDchInfo <BmnDchDigit> (detData, detQa, detName);
+        } else if (detName.Contains("MWPC")) {
+            GetCommonInfo <BmnMwpcDigit> (detData, detQa, detName);
+            GetMwpcDchInfo <BmnMwpcDigit> (detData, detQa, detName);
+        }
+    }
 
     // Calorim. dets
-    GetCommonInfo <BmnECALDigit> (fECALDigits, ecal, "ECAL");
-    GetCommonInfo <BmnZDCDigit> (fZDCDigits, zdc, "ZDC");
+    for (Int_t iDet = nCoordinate + nTime; iDet < nCoordinate + nTime + nCalorimeter; iDet++) {
+        TString detName = fSteering->GetDetectors(period, setup)[iDet];
+        TClonesArray* detData = DETECTORS[iDet];
+        BmnCalorimeterDetQa* detQa = calorimeter[iDet - nCoordinate - nTime];
+
+        if (detName.Contains("ECAL")) {
+            GetCommonInfo <BmnECALDigit> (detData, detQa, detName);
+        } else if (detName.Contains("ZDC")) {
+            GetCommonInfo <BmnZDCDigit> (detData, detQa, detName);
+        }
+    }
 
     // Trig. dets
     for (auto it : fTrigCorr)
--- QA/offline/BmnQaOffline.h
+++ QA/offline/BmnQaOffline.h
@@ -17,6 +17,8 @@
 #include <BmnSiliconTrack.h>
 #include <BmnGemTrack.h>
 
+#include <BmnOfflineQaSteering.h>
+
 #include <BmnCoordinateDetQa.h>
 #include <BmnTimeDetQa.h>
 #include <BmnCalorimeterDetQa.h>
@@ -52,100 +54,21 @@ private:
     static Int_t fCurrentEvent;
     Int_t fNEvents;
 
-    TString* fDetectors;
-    TDirectory** fDirectories;
-
     FairRootManager* ioman;
     BmnEventHeader* fBmnHeader;
-
-    // Common detectors (BM@N + SRC) - GEM, SILICON, TOF400, DCH, MWPC, TOF700, ECAL, ZDC, CSC 
-    TString fGem;
-    TString fCsc;
-    TString fSi;
-    TString fTOF400;
-    TString fDch;
-    TString fMwpc;
-    TString fTOF700;
-    TString fECAL;
-    TString fZDC;
-
-    TClonesArray* fGemDigits;
-    TClonesArray* fCscDigits;
-    TClonesArray* fSiDigits;
-    TClonesArray* fTOF400Digits;
-    TClonesArray* fDchDigits;
-    TClonesArray* fMwpcDigits;
-    TClonesArray* fTOF700Digits;
-    TClonesArray* fECALDigits;
-    TClonesArray* fZDCDigits;
-
-    // Common triggers (BM@N + SRC) - BC1, BC2, VC
-    TString fBC1;
-    TString fBC2;
-    TString fVeto;
-
-    TClonesArray* fBC1Digits;
-    TClonesArray* fBC2Digits;
-    TClonesArray* fVetoDigits;
-
-    // SRC triggers - BC3, BC4, X1L, X2L, Y1L, Y2L, X1R, X2R, Y1R, Y2R, TQDC_BC1, TQDC_BC2, TQDC_BC3, TQDC_BC4, TQDC_VETO
-    TString fBC3;
-    TString fBC4;
-    TString fX1L;
-    TString fX2L;
-    TString fY1L;
-    TString fY2L;
-    TString fX1R;
-    TString fX2R;
-    TString fY1R;
-    TString fY2R;
-    TString fTQDC_BC1;
-    TString fTQDC_BC2;
-    TString fTQDC_BC3;
-    TString fTQDC_BC4;
-    TString fTQDC_Veto;
-
-    TClonesArray* fBC3Digits;
-    TClonesArray* fBC4Digits;
-    TClonesArray* fX1LDigits;
-    TClonesArray* fX2LDigits;
-    TClonesArray* fY1LDigits;
-    TClonesArray* fY2LDigits;
-    TClonesArray* fX1RDigits;
-    TClonesArray* fX2RDigits;
-    TClonesArray* fY1RDigits;
-    TClonesArray* fY2RDigits;
-    TClonesArray* fTQDC_BC1Digits;
-    TClonesArray* fTQDC_BC2Digits;
-    TClonesArray* fTQDC_BC3Digits;
-    TClonesArray* fTQDC_BC4Digits;
-    TClonesArray* fTQDC_VetoDigits;
-
-    // BM@N triggers
-    TString fSiTrig;
-    TString fBD;
-
-    TClonesArray* fSiTrigDigits;
-    TClonesArray* fBDDigits;
-
-    BmnCoordinateDetQa* gem;
-    BmnCoordinateDetQa* silicon;
-    BmnCoordinateDetQa* csc;
-
-    BmnTimeDetQa* tof400;
-    BmnTimeDetQa* tof700;
-    BmnTimeDetQa* dch;
-    BmnTimeDetQa* mwpc;
-
-    BmnCalorimeterDetQa* ecal;
-    BmnCalorimeterDetQa* zdc;
-
-    BmnTrigDetQa* triggers;
-
-    TClonesArray** fTriggers;
+    
+    TClonesArray** DETECTORS;
+    TClonesArray** TRIGGERS;
+ 
     map <TClonesArray*, TString> fTrigCorr;
     
     Bool_t isDstRead;
+    
+    Int_t nDets;   
+    Int_t nCoordinate;
+    Int_t nTime;
+    Int_t nCalorimeter;
+        
     TChain* fChainDst;
     TClonesArray* fSiliconHits;
     TClonesArray* fSiliconTracks;
@@ -153,9 +76,20 @@ private:
     TClonesArray* fGemTracks;
     TClonesArray* fVertex;
     TClonesArray* fGlobalTracks;
-    
+   
+    // Qa-classes
+    BmnCoordinateDetQa** coordinate;
+    BmnTimeDetQa** time;
+    BmnCalorimeterDetQa** calorimeter;
+    BmnTrigDetQa* triggers;
     BmnDstQa* dst;
-
+    
+    Int_t period;
+    TString setup;
+    
+    // Steering for the QA-system
+    BmnOfflineQaSteering* fSteering;
+    
 private:
     Bool_t ReadDstTree(TString);
 

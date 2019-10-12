79b950893681a5cc4fc1472e57fcdf10c8fe4af9
Pavel Batyuk <pavel.batyuk@jinr.ru>
2019-10-12 21:26:16 +0300

    Added compatibility of the QA-system to RUN6,7 (BM@N and SRC)

--- macro/miscellaneous/offlineQA.C
+++ macro/miscellaneous/offlineQA.C
@@ -7,11 +7,13 @@
 R__ADD_INCLUDE_PATH($VMCWORKDIR)
 #include "macro/run/bmnloadlibs.C"
 
-void offlineQA(TString digiFile = "", 
-        TString dstBMN = "", 
-        TString dstCBM = "", 
-        TString out = "", 
-        Int_t nEvents = 1e6) { 
+void offlineQA(TString digiFile = "/nica/mpd22/batyuk/digi/run6/bmn_run1558_digi.root",
+//        TString dstBMN = "/nica/mpd22/batyuk/srcDST/srcdst_3387.root",
+        TString dstBMN = "",
+        Int_t periodId = 6,
+        Int_t runId = 1558,
+        TString out = "qa_1558.root",
+        Int_t nEvents = 1e6) {
 
     bmnloadlibs(); // load libraries
 
@@ -20,26 +22,29 @@ void offlineQA(TString digiFile = "",
     timer.Start();
     FairRunAna* fRunAna = new FairRunAna();
     fRunAna->SetEventHeaderPersistence(kFALSE);
-    //fRunAna->SetEventHeader(new BmnEventHeader());
-    
-    // fRunAna->SetInputFile(digiFile);
-    
-    // fRunAna->AddFriend(dstCBM);
-    // fRunAna->SetOutputFile(out.Data());
-    
+
     FairSource* fFileSource = new BmnFileSource(digiFile.Data());
     fRunAna->SetSource(fFileSource);
-    // fRunAna->SetInputFile(dstBMN);
-    // fRunAna->AddFriend(dstBMN);
-    
+
     fRunAna->SetOutputFile(out.Data());
-    
+
+    UniDbRun* pCurrentRun = UniDbRun::GetRun(periodId, runId); // FIXME
+    Double_t* field_voltage = pCurrentRun->GetFieldVoltage();
+   
+    Double_t map_current = 55.87;
+    Double_t fieldScale = (*field_voltage < 10.) ? 0 : (*field_voltage) / map_current;
+
+    BmnFieldMap* magField = new BmnNewFieldMap("field_sp41v4_ascii_Extrap.root");
+    magField->SetScale(fieldScale);
+    magField->Init();
+    fRunAna->SetField(magField);
+
     BmnQaOffline* qaSystem = new BmnQaOffline(dstBMN);
     fRunAna->AddTask(qaSystem);
-    
+
     fRunAna->Init();
     fRunAna->Run(0, nEvents);
-    
-    delete qaSystem;   
+
+    delete qaSystem;
 }
 

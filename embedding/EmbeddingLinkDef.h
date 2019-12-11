9b3093be28d238eedd061bd12d4e9b4571a9df50
Pavel Batyuk <pavel.batyuk@jinr.ru>
2019-12-11 17:41:31 +0300

    CMakeLists and LinkDef for embedding

---
+++ embedding/EmbeddingLinkDef.h
@@ -0,0 +1,11 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class BmnLambdaEmbedding+;
+#pragma link C++ class BmnLambdaStore+;
+#pragma link C++ class BmnLambdaMisc+;
+
+#endif

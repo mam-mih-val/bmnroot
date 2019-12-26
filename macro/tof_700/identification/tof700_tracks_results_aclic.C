c8746ac3c1749cfbe8e4f7ffae9638d311bba096
Yury Petukhov <yuri.petukhov@ihep.ru>
2019-12-26 19:31:32 +0300

    Delete tof700_tracks_results_aclic.C

--- macro/tof_700/identification/tof700_tracks_results_aclic.C
+++
@@ -1,8 +0,0 @@
-void tof700_tracks_results_aclic(Int_t runnumber = 1889)
-{
-  gROOT->ProcessLine(".x $VMCWORKDIR/macro/run/bmnloadlibs.C");
-  gSystem->SetIncludePath(" -Wno-sign-compare -Wno-overloaded-virtual -I$SIMPATH/include -I$SIMPATH/include/root -I$VMCWORKDIR/base/field -I$VMCWORKDIR/base/steer -I$VMCWORKDIR/base/sim -I$VMCWORKDIR/base/event -I$VMCWORKDIR/base/source -I$VMCWORKDIR/geobase -I$VMCWORKDIR/parbase -I$VMCWORKDIR/fairtools -I$VMCWORKDIR/bmndata -I$VMCWORKDIR/globaltracking -I$VMCWORKDIR/gem -I$VMCWORKDIR/silicon -I$VMCWORKDIR/uni_db -I$VMCWORKDIR/uni_db/db_classes");
-  gROOT->ProcessLine(".L BmnTracksTOF700.C++");
-//  gROOT->ProcessLine(".include");
-  BmnTracksTOF700(runnumber);
-}

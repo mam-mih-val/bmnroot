50b70451db26382178b40057b4f1d6f0b21fb5ac
Maria Patsyuk <mpatsyuk@jinr.ru>
2020-01-15 12:59:57 +0300

    added arm triggers classes (forgot them last time)

---
+++ armTriggers/BmnArmTrigLinkDef.h
@@ -0,0 +1,13 @@
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class  BmnArmTrig+;
+#pragma link C++ class  BmnArmTrigPoint+;
+//#pragma link C++ class  BmnFDGeo+;
+//#pragma link C++ class  BmnFDGeoPar+;
+//#pragma link C++ class  BmnFDContFact+;
+#endif

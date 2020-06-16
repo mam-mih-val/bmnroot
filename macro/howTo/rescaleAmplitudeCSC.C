b70bf4adf18a3da36dee2ec91b700402be75ee9e
Ilnur Gabdrakhmanov <ilnur@jinr.ru>
2020-06-17 01:07:31 +0300

    rescale samples for Si, CSC added, rescaling func updated

--- macro/howTo/rescaleAmplitudeCSC.C
+++ macro/howTo/rescaleAmplitudeCSC.C
@@ -21,19 +21,19 @@ R__ADD_INCLUDE_PATH($VMCWORKDIR)
 
 /**
  * rescaleAmplitudes
- * Example of ADC signal rescaling procedures for GEM strip digits
+ * Example of ADC signal rescaling procedures for CSC strip digits
  * @param fileNameMC
  * @param fileNameEx
  * @param fileNameDST
  */
-void rescaleAmplitude(TString fileNameMC, TString fileNameEx, TString fileNameDST) {
+void rescaleAmplitudeCSC(TString fileNameMC, TString fileNameEx, TString fileNameDST) {
     if (fileNameMC == "" || fileNameEx == "") {
         cout << "Files need to be specified!" << endl;
         return;
     }
     gStyle->SetOptStat(0);
     Int_t ClusterSizeThr = 0;
-    Double_t lowThr = 0;
+    Double_t lowThr = 1;
     Int_t xBins = 400;
     Double_t xLow = 0.0;
     Double_t xUp = 2300.0;
@@ -49,7 +49,7 @@ void rescaleAmplitude(TString fileNameMC, TString fileNameEx, TString fileNameDS
     Long64_t NEventsMC = chainMC->GetEntries();
     printf("#recorded  MC entries = %lld\n", NEventsMC);
     TClonesArray * gemMC = nullptr;
-    chainMC->SetBranchAddress("BmnGemStripDigit", &gemMC);
+    chainMC->SetBranchAddress("BmnCSCDigit", &gemMC);
 
     // Load Exp digits
     TChain* chainEx = new TChain("bmndata");
@@ -57,7 +57,7 @@ void rescaleAmplitude(TString fileNameMC, TString fileNameEx, TString fileNameDS
     Long64_t NEventsEx = chainEx->GetEntries();
     printf("#recorded Exp entries = %lld\n", NEventsEx);
     TClonesArray * gemEx = nullptr;
-    chainEx->SetBranchAddress("GEM", &gemEx);
+    chainEx->SetBranchAddress("CSC", &gemEx);
 
     // Load tracks
     TChain* chainDST = new TChain("bmndata");
@@ -68,33 +68,33 @@ void rescaleAmplitude(TString fileNameMC, TString fileNameEx, TString fileNameDS
     TClonesArray * gemTracks = nullptr;
     TClonesArray * gemHits = nullptr;
     chainDST->SetBranchAddress("BmnGlobalTrack", &tracks);
-    chainDST->SetBranchAddress("BmnGemTrack", &gemTracks);
-    chainDST->SetBranchAddress("BmnGemStripHit", &gemHits);
+//    chainDST->SetBranchAddress("BmnSiliconTrack", &gemTracks);
+    chainDST->SetBranchAddress("BmnCSCHit", &gemHits);
 
     TF1 *mc = BmnRecoTools::GetSignalDistribution(chainMC, gemMC,
             nullptr, nullptr, nullptr, nullptr,
             lowThr, 0, 2e6);
     TF1 *ex = BmnRecoTools::GetSignalDistribution(chainEx, gemEx,
             chainDST, gemHits, gemTracks, tracks,
-            0, ClusterSizeThr, 2e6);
-    TF1 *funcRescale = BmnRecoTools::GetRescaleFunc(TString("RescaleGEM"), mc, ex);
+            0, ClusterSizeThr, 2e5);
+    TF1 *funcRescale = BmnRecoTools::GetRescaleFunc(TString("RescaleCSC"), mc, ex);
     printf("rescaling func created\n");
 
-    TString title = "GEM MC  Rescaled";
+    TString title = "CSC MC  Rescaled";
     TH1D* hGemMC = new TH1D("mc", title, xBins, xLow, xUp);
-    title = "GEM Exp ";
+    title = "CSC Exp ";
     TH1D* hGemEx = new TH1D("digs", title, xBins, xLow, xUp);
-    title = "GEM Exp Hits' Digits";
+    title = "CSC Exp Hits' Digits";
     TH1D* hGemExHits = new TH1D("digs-hits", title, xBins, xLow, xUp);
-    title = "GEM Exp Cluster Signal Lower";
+    title = "CSC Exp Cluster Signal Lower";
     TH1D* hGemExCS = new TH1D("cluster-signal-upper", title, xBins, xLow, xUp);
-    title = "GEM Exp Tracks' Digits";
+    title = "CSC Exp Tracks' Digits";
     TH1D* hGemExTracks = new TH1D("digs-tracks", title, xBins, xLow, xUp);
     vector < TH1D* > hGemCluster;
     for (Int_t i = 0; i < nCSize; i++) {
-        title = "GEM Cluster Size";
+        title = "CSC Cluster Size";
         TH1D* h = new TH1D(
-                Form("gem csize %.d", i),
+                Form("csc csize %.d", i),
                 title,
                 xBins, xLow, 1000);
         hGemCluster.push_back(h);
@@ -118,7 +118,7 @@ void rescaleAmplitude(TString fileNameMC, TString fileNameEx, TString fileNameDS
 
         /** digs from hits*/
         for (Int_t iHit = 0; iHit < gemHits->GetEntriesFast(); iHit++) {
-            BmnGemStripHit *hit = (BmnGemStripHit *) gemHits->UncheckedAt(iHit);
+            BmnCSCHit *hit = (BmnCSCHit *) gemHits->UncheckedAt(iHit);
             // fill cluster signal for different cluster sizes
             hGemCluster[0]->Fill(hit->GetStripTotalSignalInLowerLayer());
             Int_t csize = hit->GetClusterSizeInLowerLayer();
@@ -151,42 +151,18 @@ void rescaleAmplitude(TString fileNameMC, TString fileNameEx, TString fileNameDS
             dig->SetName("");
         }
 
-        /** digs from tracks*/
-        for (Int_t iTrack = 0; iTrack < tracks->GetEntriesFast(); iTrack++) {
-            BmnGlobalTrack* track = (BmnGlobalTrack*) tracks->UncheckedAt(iTrack);
-            if (track->GetGemTrackIndex() != -1) {
-                //                for (Int_t iTrack = 0; iTrack < gemTrack->GetEntriesFast(); iTrack++) {
-                BmnTrack * gemTr = (BmnTrack*) gemTracks->UncheckedAt(track->GetGemTrackIndex());
-                for (Int_t iHit = 0; iHit < gemTr->GetNHits(); iHit++) {
-                    BmnGemStripHit *hit = (BmnGemStripHit *) gemHits->UncheckedAt(gemTr->GetHitIndex(iHit));
-
-                    BmnMatch match = hit->GetDigitNumberMatch();
-                    const vector<BmnLink> links = match.GetLinks();
-                    for (const BmnLink& link : links) {
-                        if (link.GetIndex() > (gemEx->GetEntriesFast() - 1)) {
-                            printf("Warning in event %lld!Tracks Link index out of range! index = %d  entries %d\n",
-                                    iEv, link.GetIndex(), gemEx->GetEntriesFast());
-                            continue;
-                        }
-                        BmnStripDigit * dig = (BmnStripDigit*) gemEx->At(link.GetIndex());
-                        hGemExTracks->Fill(dig->GetStripSignal());
-                    }
-                }
-                //                }
-            }
-        }
     }
     printf("Exp histograms filled\n");
 
 
-    title = "GEM-rescaled";
+    title = "CSC-rescaled";
     TCanvas* can = new TCanvas(title, title, 1600, 900);
     can->SetLogy();
     hGemEx->SetLineColor(kBlack);
     if (hGemEx->Integral() > 0)
         hGemEx->Scale(1 / hGemEx->Integral());
-    hGemEx->SetMinimum(8e-5);
-    hGemEx->SetMaximum(1.2e-1);
+    hGemEx->SetMinimum(1e-4);
+    hGemEx->SetMaximum(9e-2);
     hGemEx->Draw();
     hGemExHits->SetLineColor(kBlue);
 //    hGemExHits->DrawNormalized("same");
@@ -222,11 +198,11 @@ void rescaleAmplitude(TString fileNameMC, TString fileNameEx, TString fileNameDS
     legend->AddEntry(hGemMC, "MC rescaled by all exp digits");
     legend->Draw();
 
-    can->Print(Form("%s-th-%.1f-cs-%d-Lower-2m2m.pdf", can->GetName(), lowThr, ClusterSizeThr));
+    can->Print(Form("%s-th-%.1f-cs-%d-Lower-2m.pdf", can->GetName(), lowThr, ClusterSizeThr));
     can->Clear();
 
     can->SetLogy(kFALSE);
-    auto legendCluster = new TLegend(0.6, 0.65, 0.8, 0.9);
+    auto legendCluster = new TLegend(0.4, 0.65, 0.6, 0.9);
     for (Int_t i = 0; i < nCSize; i++) {
         hGemCluster[i]->SetLineColor(cols[i]);
         hGemCluster[i]->Draw(i == 0 ? "" : "same");

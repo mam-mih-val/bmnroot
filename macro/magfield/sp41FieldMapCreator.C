225192595b6f9311f1b5d6c0e2b5a30d0c5aa8a8
Pavel Batyuk <pavel.batyuk@jinr.ru>
2020-12-23 12:10:50 +0300

    Macro to be used as a field creator in the desirable axis / range

--- macro/magfield/sp41FieldMapCreator.C
+++ macro/magfield/sp41FieldMapCreator.C
@@ -2,22 +2,23 @@
 R__ADD_INCLUDE_PATH($VMCWORKDIR)
 #include "macro/run/bmnloadlibs.C"
 
-void sp41FieldMapCreator(TString in = "field_sp41v2_ascii_noExtrap.dat", TString out = "testMap_200x200.dat") {
-    bmnloadlibs(); // load libraries    
-
-    // field_sp41v2_ascii_noExtrap.dat - is an initial field map to be read and extrapolated ($VMCWORKDIR/input)
-    BmnSP41FieldMapCreator* newField = new BmnSP41FieldMapCreator(in);
-    newField->SetDebug(kTRUE);
-    newField->SetNodeNumber(200);
-    newField->SetDrawProfiles(kTRUE);
-   
-    const Int_t nParts = 2; // Left and right side ( < -28. and > 360. cm)
-    const Int_t nComponents = 3; // x -> 0, y -> 1, z -> 2
+void sp41FieldMapCreator(TString in = "", 
+        TString out1 = "", 
+        TString out2 = "") {
     
-    for (Int_t iPart = 0; iPart < nParts; iPart++)
-        for (Int_t iComp = 0; iComp < nComponents; iComp++)
-            newField->AnalyzeFieldProfiles(iPart, iComp);
+    // x
+    BmnSP41FieldMapCreator* x = new BmnSP41FieldMapCreator(in, out1);
+    x->SetDebug(kTRUE);   
+    x->SetExtrapolationAxis("X");
+    x->SetNewRanges(-250., +250.);
+    x->Extrapolation();    
+    delete x;
     
-    newField->CreateNewMap(out);
-    delete newField;
+    // z
+    BmnSP41FieldMapCreator* z = new BmnSP41FieldMapCreator(out1, out2);
+    z->SetDebug(kTRUE);   
+    z->SetExtrapolationAxis("Z");
+    z->SetNewRanges(-200, +500.);
+    z->Extrapolation();    
+    delete z;  
 }

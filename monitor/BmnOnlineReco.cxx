4f937b53ae1769c102b98b7d102575ac880d19bf
Ilnur Gabdrakhmanov <ilnur@jinr.ru>
2021-10-01 14:57:48 +0300

    Working draft for the online track reconstruction implemented

--- monitor/BmnOnlineReco.cxx
+++ monitor/BmnOnlineReco.cxx
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <ctime>
 #include <fcntl.h>
+#include <TRandom2.h>
 
 #include "UniDbRun.h"
 #include "BmnOnlineReco.h"
@@ -24,7 +25,6 @@ BmnOnlineReco::BmnOnlineReco() {
     CurRun = new BmnRunInfo();
     runPub->Add((TObject*) CurRun);
 
-    fDecoSource = new BmnDecoSource();
 
     fPeriodID = 7;
     fRunID = 4649;
@@ -32,16 +32,19 @@ BmnOnlineReco::BmnOnlineReco() {
     Bool_t isTarget = kTRUE; //kFALSE; // flag for tracking (run with target or not)
     Bool_t isExp = kFALSE; // flag for hit finder (to create digits or take them from data-file)
     // Verbosity level (0=quiet, 1=event-level, 2=track-level, 3=debug)
-    Int_t iVerbose = 2;
+    Int_t iVerbose = 0;
     Double_t fieldScale = 0.;
-    TString inputFileName = TString(getenv("VMCWORKDIR")) + "/macro/run/evetest.root";
-    TString alignCorrFileName = "default";
-    TString steerGemTrackingFile = "gemTrackingSteer.dat";
+    
+    
     fRunAna = new FairRunAna();
+    fDecoSource = new BmnDecoSource();
+    fRunAna->SetSource(fDecoSource);
     fRunAna->SetOutputFile(TString(getenv("VMCWORKDIR")) + "/macro/run/bmndst.root");
     fRunAna->SetGenerateRunInfo(false); // set magnet field with factor corresponding to the given run
-    // Get Geometry
-    TString geoFileName = "current_geo_file.root";
+    // get geometry for run
+    TRandom2 * ran = new TRandom2(0);
+//    gRandom->SetSeed(0);
+    TString geoFileName = Form("current_geo_file_%d.root", UInt_t(ran->Integer(UINT32_MAX)));
     Int_t res_code = UniDbRun::ReadGeometryFile(fPeriodID, fRunID, (char*) geoFileName.Data());
     if (res_code != 0) {
         cout << "Geometry file can't be read from the database" << endl;
@@ -101,13 +104,7 @@ BmnOnlineReco::BmnOnlineReco() {
     cout << "||\t\t\t\t\t\t\t||" << endl;
     cout << "||||||||||||||||||||||||||||||||||||||||||||||||||||||||||\n\n" << endl;
     fRunAna->SetField(magField);
-    fRunAna->SetSource(fDecoSource);
-    TList* parFileNameList = new TList();
-    TObjString stsDigiFile = TObjString(Form("%s%s", getenv("VMCWORKDIR"), "/parameters/sts_v15a_gem.digi.par"));
-    parFileNameList->Add(&stsDigiFile);
-
-    TObjString tofDigiFile = TObjString(Form("%s%s", getenv("VMCWORKDIR"), "/parameters/tof_standard.geom.par"));
-    parFileNameList->Add(&tofDigiFile);
+    //    TList* parFileNameList = new TList();
 
     //    if (iVerbose == 0) { // print only progress bar in terminal in quiet mode
     //        BmnCounter* cntr = new BmnCounter(nEvents);
@@ -124,24 +121,25 @@ BmnOnlineReco::BmnOnlineReco() {
     //    BmnMwpcHitFinder* mwpcHM = new BmnMwpcHitFinder(isExp);
     //    mwpcHM->SetUseDigitsInTimeBin(kFALSE);
     //    fRunAna->AddTask(mwpcHM);
-    //    // ====================================================================== //
-    //    // ===                         Silicon hit finder                     === //
-    //    // ====================================================================== //
-    //    BmnSiliconConfiguration::SILICON_CONFIG si_config;
-    //    switch (run_period) {
-    //        case 6: //BM@N RUN-6
-    //            si_config = BmnSiliconConfiguration::RunSpring2017;
-    //            break;
-    //        case 7: //BM@N RUN-7
-    //            si_config = BmnSiliconConfiguration::RunSpring2018;
-    //            break;
-    //        default:
-    //            si_config = BmnSiliconConfiguration::RunSpring2017;
-    //            //si_config = BmnSiliconConfiguration::RunSpring2018;
-    //    }
-    //    BmnSiliconHitMaker* siliconHM = new BmnSiliconHitMaker(isExp);
-    //    siliconHM->SetCurrentConfig(si_config);
-    //    fRunAna->AddTask(siliconHM);
+    // ====================================================================== //
+    // ===                         Silicon hit finder                     === //
+    // ====================================================================== //
+    BmnSiliconConfiguration::SILICON_CONFIG si_config;
+    switch (fPeriodID) {
+        case 6: //BM@N RUN-6
+            si_config = BmnSiliconConfiguration::RunSpring2017;
+            break;
+        case 7: //BM@N RUN-7
+            si_config = BmnSiliconConfiguration::RunSpring2018;
+            break;
+        default:
+            si_config = BmnSiliconConfiguration::RunSpring2017;
+            //si_config = BmnSiliconConfiguration::RunSpring2018;
+    }
+    BmnSiliconHitMaker* siliconHM = new BmnSiliconHitMaker(fPeriodID, fRunID, isExp);
+    siliconHM->SetCurrentConfig(si_config);
+    siliconHM->SetVerbose(iVerbose);
+    fRunAna->AddTask(siliconHM);
     // ====================================================================== //
     // ===                         GEM hit finder                         === //
     // ====================================================================== //
@@ -163,24 +161,33 @@ BmnOnlineReco::BmnOnlineReco() {
     BmnGemStripHitMaker* gemHM = new BmnGemStripHitMaker(fPeriodID, fRunID, isExp);
     gemHM->SetCurrentConfig(gem_config);
     gemHM->SetHitMatching(kTRUE);
+    gemHM->SetVerbose(iVerbose);
     fRunAna->AddTask(gemHM);
 
     // ====================================================================== //
     // ===                           ALIGNMENT (GEM + SI)                 === //
     // ====================================================================== //
+    //====================================================================== //
+    // ===                          TOF1 hit finder                       === //
+    // ====================================================================== //
+    BmnTof1HitProducer* tof1HP = new BmnTof1HitProducer("TOF1", !isExp, iVerbose, kFALSE);
+    tof1HP->SetPeriodRun(fPeriodID, fRunID);
+    fRunAna->AddTask(tof1HP);
 
-    //    // ====================================================================== //
-    //    // ===                           TOF1 hit finder                      === //
-    //    // ====================================================================== //
-    //    BmnTof1HitProducer* tof1HP = new BmnTof1HitProducer("TOF1", !isExp, iVerbose, kTRUE);
-    //    tof1HP->SetPeriod(run_period);
-    //    //tof1HP->SetOnlyPrimary(kTRUE);
-    //    fRunAna->AddTask(tof1HP);
-    //    // ====================================================================== //
-    //    // ===                           TOF2 hit finder                      === //
-    //    // ====================================================================== //
-    //    BmnTofHitProducer* tof2HP = new BmnTofHitProducer("TOF", "TOF700_geometry_run6.txt", !isExp, iVerbose, kTRUE);
-    //    //    fRunAna->AddTask(tof2HP);
+    // ====================================================================== //
+    // ===                          TOF2 hit finder                       === //
+    // ====================================================================== //
+    BmnTofHitProducer* tof2HP = new BmnTofHitProducer("TOF", "TOF700_geometry_run7.txt", !isExp, iVerbose, kFALSE);
+    tof2HP->SetTimeResolution(0.115);
+    tof2HP->SetProtonTimeCorrectionFile("bmn_run9687_digi_calibration.root");
+    tof2HP->SetMCTimeFile("TOF700_MC_argon_qgsm_time_run7.txt");
+    tof2HP->SetMainStripSelection(0); // 0 - minimal time, 1 - maximal amplitude
+    tof2HP->SetSelectXYCalibration(2); // 0 - Petukhov, 1 - Panin SRC, 2 - Petukhov Argon (default)
+    tof2HP->SetTimeMin(-2.f); // minimal digit time
+    tof2HP->SetTimeMax(+39.f); // Maximal digit time
+    tof2HP->SetDiffTimeMaxSmall(1.2f); // Abs maximal difference for small chambers
+    tof2HP->SetDiffTimeMaxBig(3.5f); // Abs maximal difference for big chambers
+    fRunAna->AddTask(tof2HP);
     //    // ====================================================================== //
     //    // ===                           Tracking (MWPC)                      === //
     //    // ====================================================================== //
@@ -200,6 +207,25 @@ BmnOnlineReco::BmnOnlineReco() {
     //    BmnDchTrackFinder* dchTF = new BmnDchTrackFinder(isExp);
     //    dchTF->SetTransferFunction("pol_coord00813.txt");
     //    //    fRunAna->AddTask(dchTF);
+    
+    
+    // ====================================================================== //
+    // ===                          CSC hit finder                        === //
+    // ====================================================================== //
+    BmnCSCHitMaker* cscHM = new BmnCSCHitMaker(fPeriodID, fRunID, isExp);
+    if (!isExp)
+        cscHM->SetCurrentConfig(BmnCSCConfiguration::RunSpring2018); //set explicitly
+    cscHM->SetHitMatching(kTRUE);
+//    cscHM->SetVerbose(iVerbose);
+    fRunAna->AddTask(cscHM);
+
+    // ====================================================================== //
+    // ===                       Tracking (InnerTracker)                  === //
+    // ====================================================================== //
+    BmnInnerTrackingRun7* innerTF = new BmnInnerTrackingRun7(fRunID, isField, isTarget);
+    innerTF->SetFiltration(isExp); //we use filtration for experimental data only now
+    fRunAna->AddTask(innerTF);
+    
     // ====================================================================== //
     // ===                          Global Tracking                       === //
     // ====================================================================== //
@@ -208,27 +234,23 @@ BmnOnlineReco::BmnOnlineReco() {
     //    fRunAna->AddTask(globalTF);
 
     // ====================================================================== //
-    // ===                     Primary vertex finding                     === //
+    // ===                      Primary vertex finding                    === //
     // ====================================================================== //
-    //    BmnVertexFinder* gemVF = new BmnVertexFinder(fPeriodID, isField);
-    //    // gemVF->SetVertexApproximation(TVector3(0., 0., 0.));
-    //    fRunAna->AddTask(gemVF);
+    BmnVertexFinder* gemVF = new BmnVertexFinder(fRunID, isField);
+    fRunAna->AddTask(gemVF);
 
-    // Residual analysis
-    //    if (isExp) {
-    //        BmnGemResiduals* residAnalGem = new BmnGemResiduals(fPeriodID, fRunID, fieldScale);
-    //        // residAnal->SetPrintResToFile("file.txt");
-    //        // residAnal->SetUseDistance(kTRUE); // Use distance instead of residuals
-    //        fRunAna->AddTask(residAnalGem);
-    //        // BmnSiResiduals* residAnalSi = new BmnSiResiduals(run_period, run_number, fieldScale);
-    //        // fRunAna->AddTask(residAnalSi);
-    //    }
+    // Fill DST Event Header (if iVerbose = 0, then print progress bar)
+    BmnFillDstTask* dst_task = new BmnFillDstTask();
+    dst_task->SetRunNumber(fPeriodID, fRunID);
+    dst_task->SetVerbose(2);
+    fRunAna->AddTask(dst_task);
+    
     // -----   Parameter database   --------------------------------------------
     FairRuntimeDb* rtdb = fRunAna->GetRuntimeDb();
     FairParRootFileIo* parIo1 = new FairParRootFileIo();
     FairParAsciiFileIo* parIo2 = new FairParAsciiFileIo();
-    parIo1->open(inputFileName.Data());
-    parIo2->open(parFileNameList, "in");
+    //    parIo1->open(inputFileName.Data());
+    //    parIo2->open(parFileNameList, "in");
     //    rtdb->setFirstInput(parIo1);
     rtdb->setSecondInput(parIo2);
     rtdb->setOutput(parIo1);
@@ -249,8 +271,8 @@ BmnOnlineReco::~BmnOnlineReco() {
         zmq_ctx_destroy(_ctx);
         _ctx = NULL;
     }
-    if (CurRun)
-        delete CurRun;
+//    if (CurRun)
+//        delete CurRun;
     if (runPub)
         delete runPub;
     fDecoSource->Close();
@@ -265,7 +287,7 @@ void BmnOnlineReco::RecoStream(TString dirname, TString refDir, TString decoAddr
 
 void BmnOnlineReco::ProcessDigi(Int_t iEv) {
     fEvents++;
-//    BmnEventHeader* head = fDigiArrays->header;
+    //    BmnEventHeader* head = fDigiArrays->header;
     if (fEvents % itersToUpdate == 0) {
     }
 }

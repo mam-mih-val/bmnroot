0836677ee576c9936f0859226c33fe1fae16a90d
Viktor <klochkov44@gmail.com>
2021-11-26 12:35:10 +0100

    add AnalysisTree interface from CBM

---
+++ analysis/common/analysis_tree_converter/CbmConverterTask.cxx
@@ -0,0 +1,7 @@
+/* Copyright (C) 2020 Physikalisches Institut, Eberhard Karls Universitaet Tuebingen, Tuebingen
+   SPDX-License-Identifier: GPL-3.0-only
+   Authors: Viktor Klochkov [committer] */
+
+#include "CbmConverterTask.h"
+
+
---
+++ analysis/common/analysis_tree_converter/CbmSimEventHeaderConverter.cxx
@@ -0,0 +1,63 @@
+/* Copyright (C) 2020-2021 Physikalisches Institut, Eberhard Karls Universitaet Tuebingen, Tuebingen
+   SPDX-License-Identifier: GPL-3.0-only
+   Authors: Viktor Klochkov [committer] */
+
+#include "CbmSimEventHeaderConverter.h"
+
+//#include "CbmEvent.h"
+
+#include "FairMCEventHeader.h"
+#include "FairRootManager.h"
+//#include "CbmMCDataManager.h"
+//#include "CbmMCDataArray.h"
+//#include "CbmMCDataObject.h"
+
+#include "TClonesArray.h"
+
+#include <AnalysisTree/TaskManager.hpp>
+
+#include "cassert"
+#include "iostream"
+
+ClassImp(CbmSimEventHeaderConverter);
+
+void CbmSimEventHeaderConverter::Init()
+{
+  assert(!out_branch_.empty());
+  auto* ioman = FairRootManager::Instance();
+  assert(ioman != nullptr);
+
+//  cbm_mc_manager_    = dynamic_cast<CbmMCDataManager*>(ioman->GetObject("MCDataManager"));
+//  cbm_header_obj_ = cbm_mc_manager_->GetObject("MCEventHeader.");
+
+  //  ***** SimEventHeader *******
+  AnalysisTree::BranchConfig SimEventHeaderBranch("SimEventHeader", AnalysisTree::DetType::kEventHeader);
+  SimEventHeaderBranch.AddField<float>("psi_RP", "reaction plane orientation");
+  SimEventHeaderBranch.AddField<float>("b", "fm, impact parameter");
+  SimEventHeaderBranch.AddFields<float>({"start_time", "end_time"}, "ns (?), event time");
+  SimEventHeaderBranch.AddField<int>("run_id", "run identifier");
+  SimEventHeaderBranch.AddField<int>("event_id", "event identifier");
+
+  auto* man = AnalysisTree::TaskManager::GetInstance();
+  man->AddBranch(out_branch_, sim_event_header_, SimEventHeaderBranch);
+  sim_event_header_->Init(SimEventHeaderBranch);
+}
+
+void CbmSimEventHeaderConverter::ProcessData()
+{
+  FairMCEventHeader* cbm_header{nullptr};
+
+  LOG(info) << "MCEvent " << cbm_header->GetEventID() << " " << cbm_header->GetT();
+
+  if (!cbm_header) { throw std::runtime_error("CbmSimEventHeaderConverter::Exec - ERROR! No fHeader!"); }
+  auto* out_config_  = AnalysisTree::TaskManager::GetInstance()->GetConfig();
+  const auto& branch = out_config_->GetBranchConfig(out_branch_);
+
+  TVector3 pos {cbm_header->GetX(), cbm_header->GetY(), cbm_header->GetZ()};
+  sim_event_header_->SetVertexPosition3(pos);
+
+  sim_event_header_->SetField(float(cbm_header->GetRotZ()), branch.GetFieldId("psi_RP"));
+  sim_event_header_->SetField(float(cbm_header->GetB()), branch.GetFieldId("b"));
+  sim_event_header_->SetField(int(cbm_header->GetEventID()), branch.GetFieldId("event_id"));
+  sim_event_header_->SetField(int(cbm_header->GetRunID()), branch.GetFieldId("run_id"));
+}

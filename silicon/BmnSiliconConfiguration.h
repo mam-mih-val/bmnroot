4c06e0a40f72c5cbc248b5f1de8dd2082d50af07
dbaranov <dmitriybaranof@gmail.com>
2022-01-05 17:30:41 +0300

    SILICON: configuration (5 stations: 6-10-14-18-22 mods) for RUN-8 added

--- silicon/BmnSiliconConfiguration.h
+++ silicon/BmnSiliconConfiguration.h
@@ -8,8 +8,9 @@ namespace BmnSiliconConfiguration {
         RunSpring2017,    // 1 station - (geometry file: Silicon_RunSpring2017.root) (BM@N RUN-6)
         RunSpring2018,    // 3 stations - (geometry file: Silicon_RunSpring2018.root) (BM@N RUN-7)
         RunSRCSpring2018, // 3 stations - (geometry file: Silicon_RunSRCSpring2018.root) (BM@N RUN-7: SRC)
-        Run8_3stations,  // 3 stations - (geometry files: Silicon_Run8_3stations.root and Silicon_Run8_3stations_detailed.root)
-        Run8_4stations   // 4 stations - (geometry files: Silicon_Run8_4stations.root and Silicon_Run8_4stations_detailed.root)
+        Run8_3stations,   // 3 stations - (geometry files: Silicon_Run8_3stations.root and Silicon_Run8_3stations_detailed.root)
+        Run8_4stations,   // 4 stations - (geometry files: Silicon_Run8_4stations.root and Silicon_Run8_4stations_detailed.root)
+        Run8_5stations    // 5 stations - (geometry files: Silicon_Run8_5stations.root and Silicon_Run8_5stations_detailed.root)
     };
 }
 

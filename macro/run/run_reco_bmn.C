46f3d879c9b6ef7f2f61eda69a04e7adb3594d82
Sergei Merts <sergey.merts@gmail.com>
2022-01-17 16:35:52 +0300

    another bugfix

--- macro/run/run_reco_bmn.C
+++ macro/run/run_reco_bmn.C
@@ -7,9 +7,9 @@
 R__ADD_INCLUDE_PATH($VMCWORKDIR)
 #define CellAuto // Choose Tracking: L1 or CellAuto
 
-void run_reco_bmn(TString inputFileName = "$VMCWORKDIR/macro/run/bmnsim.root",
+void run_reco_bmn(TString inputFileName = "$VMCWORKDIR/macro/run/bmn_run4600_digi.root",
         TString bmndstFileName = "$VMCWORKDIR/macro/run/bmndst.root",
-        Int_t nStartEvent = 0, Int_t nEvents = 1000)
+        Int_t nStartEvent = 0, Int_t nEvents = 10)
 {
     gDebug = 0; // Debug option
     // Verbosity level (0 = quiet (progress bar), 1 = event-level, 2 = track-level, 3 = full debug)
@@ -141,13 +141,14 @@ void run_reco_bmn(TString inputFileName = "$VMCWORKDIR/macro/run/bmnsim.root",
     // ===                         Silicon hit finder                     === //
     // ====================================================================== //
     BmnSiliconHitMaker* siliconHM = new BmnSiliconHitMaker(run_period, run_number, isExp);
+    if (isExp) siliconHM->SetHitMatching(kFALSE);
     fRunAna->AddTask(siliconHM);
 
     // ====================================================================== //
     // ===                          GEM hit finder                        === //
     // ====================================================================== //
     BmnGemStripHitMaker* gemHM = new BmnGemStripHitMaker(run_period, run_number, isExp);
-    gemHM->SetHitMatching(kTRUE);
+    if (isExp) gemHM->SetHitMatching(kFALSE);
     fRunAna->AddTask(gemHM);    
     
     // ====================================================================== //

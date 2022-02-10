89346e88d48281de47f3ed56b1a8c6b6912c855d
Yury Petukhov <yuri.petukhov@ihep.ru>
2022-02-10 14:39:08 +0300

    Delete RawToRoot.C

--- macro/tof_700/RawToRoot.C
+++
@@ -1,8 +0,0 @@
-void RawToRoot(char *fname="mpd_run_Glob_871.data")
-{
-gROOT->LoadMacro("$VMCWORKDIR/macro/run/bmnloadlibs.C");
-bmnloadlibs();
-gSystem->SetIncludePath(" -Wno-ignored-qualifiers -Wno-overloaded-virtual -Wno-unused-variable -Wno-unused-but-set-variable -Wno-unused-result -Wno-deprecated -I$ROOTSYS/include/root -I$VMCWORKDIR/bmndata");
-gROOT->ProcessLine(".L BmnData.C++");
-BmnData(fname);
-}

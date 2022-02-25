52e0180c4f6d1de9588f2b409086ed9e87ff30d5
mam-mih-val <mam.mih.val@gmail.com>
2022-02-25 18:03:27 +0100

    Fixing the converter. Checking if this works

--- analysis/common/analysis_tree_converter/CbmStsTracksConverter.h
+++ analysis/common/analysis_tree_converter/CbmStsTracksConverter.h
@@ -32,36 +32,15 @@ public:
   void ProcessData() final;
   void Finish() final {}
 
-  void SetIsWriteKFInfo(bool is = true) { is_write_kfinfo_ = is; }
-  void SetIsReproduceCbmKFPF(bool is = true) { is_reproduce_cbmkfpf_ = is; }
-
 private:
   void ReadVertexTracks();
-  void MapTracks();
   void InitInput();
-//   float ExtrapolateToVertex(CbmStsTrack* sts_track, AnalysisTree::Track& track, int pdg);
 
-  void WriteKFInfo(AnalysisTree::Track& track, CbmStsTrack* sts_track, bool is_good_track) const;
-  bool IsGoodCovMatrix(CbmStsTrack* sts_track) const;
-
-  AnalysisTree::TrackDetector* vtx_tracks_ {nullptr};   ///< raw pointers are needed for TTree::Branch
-  AnalysisTree::Matching* vtx_tracks_2_sim_ {nullptr};  ///< raw pointers are needed for TTree::Branch
-
-  CbmVertex* cbm_prim_vertex_ {nullptr};    ///< non-owning pointer
-                                            //  TClonesArray* cbm_mc_tracks_ {nullptr};   ///< non-owning pointer
-  TClonesArray* cbm_mc_tracks_ {nullptr};  ///< non-owning pointer
-  TClonesArray* cbm_sts_tracks_ {nullptr};  ///< non-owning pointer
-  TClonesArray* cbm_sts_match_ {nullptr};   ///< non-owning pointer
-
-  bool is_write_kfinfo_ {true};
-  bool is_reproduce_cbmkfpf_ {true};
-
-  int ipar_ {-1};
-  int imf_ {-1};
-  int icov_ {-1};
-  int imc_pdg_ {-1};
-  int imother_pdg_ {-1};
-  int ipasscuts_ {-1};
+  AnalysisTree::TrackDetector* vtx_tracks_{nullptr};   ///< raw pointers are needed for TTree::Branch
+  AnalysisTree::Matching* sim_particles_2_vtx_tracks_{nullptr};  ///< raw pointers are needed for TTree::Branch
+  CbmVertex* bmn_vertex_{nullptr};    ///< non-owning pointer
+                                            //  TClonesArray* bmn_mc_tracks_ {nullptr};   ///< non-owning pointer
+  TClonesArray*bmn_global_tracks_{nullptr};  ///< non-owning pointer
 
   ClassDef(CbmStsTracksConverter, 1)
 };

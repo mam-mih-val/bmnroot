18e7acc2da5067f74200aa34c2258135fd65cd4e
mam-mih-val <mam.mih.val@gmail.com>
2022-03-13 00:21:20 +0300

    Fix of the script to convert DST to AnalysisTree

--- analysis/common/macro/run_analysis_tree_maker.C
+++ analysis/common/macro/run_analysis_tree_maker.C
@@ -2,8 +2,7 @@
    SPDX-License-Identifier: GPL-3.0-only
    Authors: Viktor Klochkov, Viktor Klochkov */
 
-void run_analysis_tree_maker(TString dataSet = "data_test/", TString setupName = "sis100_electron",
-                             TString unigenFile = "")
+void run_analysis_tree_maker(TString dst_file = "data_test/", TString geant_file = "data_test/", TString output_file = "data_test/")
 {
   const std::string system = "Au+Au";  // TODO can we read it automatically?
   const float beam_mom     = 12.;
@@ -19,62 +18,32 @@ void run_analysis_tree_maker(TString dataSet = "data_test/", TString setupName =
   const TString srcDir = gSystem->Getenv("VMCWORKDIR");  // top source directory
   // ------------------------------------------------------------------------
 
-  // -----   In- and output file names   ------------------------------------
-  TString traFile           = dataSet + "sim/bmnsim_3731428_2.root";
-//   TString rawFile           = dataSet + ".raw.root";
-  TString recFile           = dataSet + "reco/bmndst_3731428_2.root";
-//   TString geoFile           = dataSet + ".geo.root";
-//   TString parFile           = dataSet + ".par.root";
-  const std::string outFile = dataSet.Data() + std::string("analysistree.root");
-//   if (unigenFile.Length() == 0) { unigenFile = srcDir + "/input/urqmd.auau.10gev.centr.root"; }
-  // ------------------------------------------------------------------------
-
   // -----   Timer   --------------------------------------------------------
   TStopwatch timer;
   timer.Start();
   // ------------------------------------------------------------------------
 
   // -----   Remove old CTest runtime dependency file  ----------------------
-  const TString dataDir  = gSystem->DirName(dataSet);
-  const TString dataName = gSystem->BaseName(dataSet);
+  const TString dataDir  = gSystem->DirName(dst_file);
+  const TString dataName = gSystem->BaseName(dst_file);
   const TString testName = ("run_treemaker");
   // ------------------------------------------------------------------------
 
-//  // -----   Load the geometry setup   -------------------------------------
-//  std::cout << std::endl;
-//  const TString setupFile  = srcDir + "/geometry/setup/setup_" + setupName + ".C";
-//  const TString setupFunct = "setup_" + setupName + "()";
-//
-//  std::cout << "-I- " << myName << ": Loading macro " << setupFile << std::endl;
-
-
-//  gROOT->LoadMacro(setupFile);
-//  gROOT->ProcessLine(setupFunct);
-//  CbmSetup* setup = CbmSetup::Instance();
-
-//  TString geoTag;
-//  auto* parFileList = new TList();
-
-//   std::cout << "-I- " << myName << ": Using raw file " << rawFile << std::endl;
-//   std::cout << "-I- " << myName << ": Using parameter file " << parFile << std::endl;
-  std::cout << "-I- " << myName << ": Using reco file " << recFile << std::endl;
-//   if (unigenFile.Length() > 0) std::cout << "-I- " << myName << ": Using unigen file " << unigenFile << std::endl;
+  std::cout << "-I- " << myName << ": Using reco file " << dst_file << std::endl;
 
   // -----   Reconstruction run   -------------------------------------------
   auto* run         = new FairRunAna();
-  auto* inputSource = new FairFileSource(recFile);
-  inputSource->AddFriend(traFile);
-//   inputSource->AddFriend(rawFile);
+  auto* inputSource = new FairFileSource(dst_file);
+  inputSource->AddFriend(geant_file);
   run->SetSource(inputSource);
-//   run->SetOutputFile(outFile.c_str());
-//   run->SetGenerateRunInfo(kTRUE);
+
   // ------------------------------------------------------------------------
 
   // AnalysisTree converter
   auto* man = new CbmConverterManager();
   man->SetSystem(system);
   man->SetBeamMomentum(beam_mom);
-  man->SetOutputName(outFile, "rTree");
+  man->SetOutputName(output_file, "rTree");
 
   man->AddTask(new CbmSimEventHeaderConverter("SimEventHeader"));
   man->AddTask(new CbmRecEventHeaderConverter("RecEventHeader"));
@@ -84,16 +53,9 @@ void run_analysis_tree_maker(TString dataSet = "data_test/", TString setupName =
   taskCbmStsTracksConverter->SetIsWriteKFInfo();
   taskCbmStsTracksConverter->SetIsReproduceCbmKFPF();
   man->AddTask(taskCbmStsTracksConverter);
-//
-//  man->AddTask(new CbmRichRingsConverter("RichRings", "VtxTracks"));
-//  man->AddTask(new CbmTofHitsConverter("TofHits", "VtxTracks"));
-//  man->AddTask(new CbmTrdTracksConverter("TrdTracks", "VtxTracks"));
-//  if(is_event_base){
-//    man->AddTask(new CbmPsdModulesConverter("PsdModules"));
-//  }
+
   run->AddTask(man);
 
-// -----   Intialise and run   --------------------------------------------
   run->Init();
 
   std::cout << "Starting run" << std::endl;
